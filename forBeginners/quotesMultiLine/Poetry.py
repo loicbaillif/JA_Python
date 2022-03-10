bb6c4d8b3c4c9c79b620393fe85d5f3733d1cc50
LoicBaillif <larmedeciel@gmail.com>
2022-03-10 19:19:49 +0100

    Poetry
    Exercise

---
+++ forBeginners/quotesMultiLine/Poetry.py
@@ -0,0 +1,13 @@
+# ****** Quotes and multi-line strings : Poetry******
+# 
+# Write a program that will create and print this string from
+# "How The Grinch Stole Christmas"
+# Did that stop the old Grinch?
+# No! The Grinch simply said,
+# "If I can't find a reindeer,
+# I'll make one instead!"
+
+print('''Did that stop the old Grinch?
+No! The Grinch simply said,
+"If I can't find a reindeer,
+I'll make one instead!"''')
