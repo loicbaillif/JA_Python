53383c1c6adffee6ef163cd1815c07546157a2e7
LoicBaillif <larmedeciel@gmail.com>
2022-03-10 19:26:12 +0100

    Triple quoted string
    Exercise

---
+++ forBeginners/quotesMultiLine/tripleQuote.py
@@ -0,0 +1,14 @@
+# ****** Quotes and multi-line strings : Triple-quoted string******
+# 
+# Create a program that will print this multi-line string containing triple 
+# double quote :
+#
+# """
+# THIS IS A STRING
+# """
+#
+# Note: quotes, too, should be printed!
+
+print('''"""
+THIS IS A STRING
+"""''')
