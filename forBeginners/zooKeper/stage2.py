a689cdf4b402d84b75dfa958d9b3407415496926
loicbaillif <larmedeciel@gmail.com>
2022-03-13 21:41:19 +0100

    Create stage2

---
+++ forBeginners/zooKeper/stage2.py
@@ -0,0 +1,35 @@
+# ****** PROJECT ZOOKEEPER : Stage 2 - Show me an anymal ******
+
+# Description :
+# One of the most important parts of working with animals is keeping an eye 
+# on them. We need to see the animals on the screen to know how they are 
+# doing, right? Now we are ready to print something awesome: an image of an 
+# animal! 
+# 
+# Objectives :
+# For the second stage, you will need to develop an animal printer. 
+# Your program should display the animal identified in the code field.
+
+
+
+
+print(r"""
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!""")
