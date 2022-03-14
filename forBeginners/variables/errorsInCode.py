bf15a10e5b84eb88505c04c7ee9fa0e19ee29b16
loicbaillif <larmedeciel@gmail.com>
2022-03-14 22:18:53 +0100

    Exercise

---
+++ forBeginners/variables/errorsInCode.py
@@ -0,0 +1,13 @@
+# ****** VARIABLES - Exercise : Errors in code ******
+
+# Imagine your friend has created a code for checking the type of 
+# variable (ost = 'Happy'), but you have found some errors in it. Help him: 
+# find two or three errors, correct the code and print 
+# the type of the variable.
+
+# Original file:
+# ost = Happy <== Typo : missing quote marks
+# print(type_of(0st)) <== Typo :0 instead of o - Typo 2 : type, not type_of
+
+ost = "Happy"
+print(type_of(ost))
