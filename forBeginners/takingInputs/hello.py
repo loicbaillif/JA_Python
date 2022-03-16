b98996c0f836a7821cf8a452182c7ff823617b31
loicbaillif <larmedeciel@gmail.com>
2022-03-16 22:00:00 +0100

    Taking input
    Exercise 2

---
+++ forBeginners/takingInputs/hello.py
@@ -0,0 +1,7 @@
+# ****** TAKING INPUT - Exercise : Hello ******
+#
+# Write a program that reads the input from the user and then prints 
+# "Hello, " + user input. Don't print any additional messages!
+
+name = input()
+print("Hello, " + name)
