e19653d1c1fa48b885df96ec5025ac88cc351a0b
loicbaillif <larmedeciel@gmail.com>
2022-03-16 22:04:17 +0100

    Taking Input
    Exercise 3

---
+++ forBeginners/takingInputs/sum.py
@@ -0,0 +1,10 @@
+# ****** TAKING INPUT - Exercise : Sum ******
+
+# Write a program that prints the sum of three integer numbers. 
+# Read each number from the input separately and don't specify 
+# any message in the input, please.
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+print(num1 + num2 + num3)
