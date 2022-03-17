2f0223d0ac3289feb4e23fc2432042bcf78e3973
loicbaillif <larmedeciel@gmail.com>
2022-03-17 21:03:39 +0100

    Exercise
    Test

---
+++ forBeginners/takingInputs/test.py
@@ -0,0 +1,11 @@
+# ****** TAKING INPUTS - Exercise : Test ******
+
+# Let's see how it works. Copy the following code and paste it below. 
+# Then change one of the lines so that the program would print any data 
+# the user inputs without performing any operations on them.
+
+# data = ...
+# print(data)
+
+# data = ...
+print(input())
