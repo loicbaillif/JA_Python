4970084b218c9ebb0aec84108baf1659ef046a58
loicbaillif <larmedeciel@gmail.com>
2022-03-17 20:53:17 +0100

    Exercise
    Difference

---
+++ forBeginners/takingInputs/difference.py
@@ -0,0 +1,10 @@
+# ****** TAKING INPUTS - Exercise : Difference ******
+
+# Write a program that reads two integer numbers from user 
+# and then prints their difference. 
+# 
+# Don't specify any message when reading the input, please.
+
+num1 = int(input())
+num2 = int(input())
+print(num1 - num2)
