5486bc7d88be1a1bf5d84cfc0a6c6fee2055f255
loicbaillif <larmedeciel@gmail.com>
2022-03-17 20:46:52 +0100

    Exercise
    Create sum of two floats

---
+++ forBeginners/takingInputs/sumOfTwoFloats.py
@@ -0,0 +1,8 @@
+# ****** TAKING INPUT - Exercise : Sum of two floats ******
+
+# Write a program that reads two float numbers from the user 
+# (use the float() function) and prints their sum.
+
+float1 = float(input())
+float2 = float(input())
+print(float1 + float2)
