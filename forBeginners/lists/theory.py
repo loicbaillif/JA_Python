e8b6b8bb3ba3700bda1d22c3fa8a018f47de9e55
loicbaillif <larmedeciel@gmail.com>
2022-03-20 15:03:31 +0100

    Theory
    Lists

---
+++ forBeginners/lists/theory.py
@@ -0,0 +1,12 @@
+# ****** LISTS - THEORY ******
+
+# Create a list:
+dog_breeds = ['corgi', 'labrador', 'huskey', 'pitbull']
+print(dog_breeds)
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# Create list using list() on iterable object
+list_of_chars = list('danger!')
+print(list_of_chars) # ['d', 'a', 'n', 'g', 'e', 'r', '!']
+# list_of_ints = list(12345) # won't work ... int is not an iterable
