43cf73f344f03d3bf140977f99a6c92529d295f7
loicbaillif <larmedeciel@gmail.com>
2022-03-20 15:35:06 +0100

    Exercise
    Reveal the hidden

---
+++ forBeginners/lists/revealHidden.py
@@ -0,0 +1,8 @@
+# ****** LISTS - Exercise : Reveal the hidden ******
+
+# There's a variable hidden, which contains a list. 
+# Count the number of elements in this list and print the length of the list.
+
+hidden = list("terces giB")
+nbElts = len(hidden)
+print(nbElts)
