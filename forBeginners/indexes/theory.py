571efc65b0d27fac23ea59b24b06a7df99a86fac
loicbaillif <larmedeciel@gmail.com>
2022-03-20 16:13:11 +0100

    Indexes Theory

---
+++ forBeginners/indexes/theory.py
@@ -0,0 +1,28 @@
+print("****** INDEXES - Theory ******\n")
+
+
+print("1) Indexes of elements")
+colours = ['red', 'yellow', 'blue', 'green']
+print("colours is a list made of", len(colours), "elements")
+print(colours)
+print("colours[0] =", colours[0])
+print("colours[1] =", colours[1])
+print("colours[2] =", colours[2])
+print("colours[3] =", colours[3])
+# A list is a sequence.
+# A string is also a sequence:
+pet = "horse"
+print("pet is a String made of", len(pet), "elements")
+print(pet)
+print("pet[0] =", pet[0])
+print("pet[1] =", pet[1])
+print("pet[2] =", pet[2])
+print("pet[3] =", pet[3])
+print("pet[4] =", pet[4])
+
+print("\n******\n")
+print("2) Negative indexes")
+print("colours[-1] =", colours[-1])
+print("pet[-1] =", pet[-1])
+print("colours[-len(colours)] =", colours[-len(colours)])
+print("pet[-len(pet)] =", pet[-len(pet)])
