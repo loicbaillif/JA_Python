48128fb032e5cecb3d04a31eb980ae0c937d5fb2
loicbaillif <larmedeciel@gmail.com>
2022-03-20 15:27:54 +0100

    Exercise
    Single-element list

---
+++ forBeginners/lists/singleElementList.py
@@ -0,0 +1,17 @@
+# ****** LISTS - Exercise : Single element list ******
+#
+# Helen wants to create a list containing only one element – a string with 
+# her name. To do this, she wrote this line of code:
+
+#     name = list('Helen')
+
+# However, when she prints the list, her name appears broken into 
+# several one-letter strings:
+
+#     print(name)
+#     # ['H', 'e', 'l', 'e', 'n']
+
+# Why? Fix this code.
+
+name = ['Helen']
+print(name)
