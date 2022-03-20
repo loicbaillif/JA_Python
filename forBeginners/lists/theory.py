b8f5edcc4eb6b541f795f495caf592391cf30d6a
loicbaillif <larmedeciel@gmail.com>
2022-03-20 15:05:50 +0100

    Update

--- forBeginners/lists/theory.py
+++ forBeginners/lists/theory.py
@@ -10,3 +10,7 @@ print(numbers)
 list_of_chars = list('danger!')
 print(list_of_chars) # ['d', 'a', 'n', 'g', 'e', 'r', '!']
 # list_of_ints = list(12345) # won't work ... int is not an iterable
+
+# It is possible to store different data types in a same list
+mixed_list = [23, 'rue de Paris', True]
+print(mixed_list)
