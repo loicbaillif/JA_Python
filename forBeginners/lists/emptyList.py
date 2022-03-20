f1f78d5084d2d74bfce5f7d961f5448abfef4f62
loicbaillif <larmedeciel@gmail.com>
2022-03-20 15:41:08 +0100

    Exercise
    Empty List

---
+++ forBeginners/lists/emptyList.py
@@ -0,0 +1,7 @@
+# ****** LITS - Exercise : Empty list ******
+
+# Write a program that will create an em_list variable. 
+# Assign an empty list to it, then print your empty list.
+
+em_list = []
+print(em_list)
