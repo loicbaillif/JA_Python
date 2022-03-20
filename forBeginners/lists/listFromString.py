a3c702953ed856fd669e67f2974086731969fa25
loicbaillif <larmedeciel@gmail.com>
2022-03-20 15:19:17 +0100

    Exercise
    List from String

---
+++ forBeginners/lists/listFromString.py
@@ -0,0 +1,10 @@
+# ****** LISTS - Exercises : List from String ******
+
+# Write a program that constructs a list from the given string's symbols 
+# and then prints it.
+
+# So, if input_str, for example, contains a string "python", your program 
+# should turn it to the list ['p', 'y', 't', 'h', 'o', 'n'] and print it.
+
+userInput = input()
+print(list(userInput))
