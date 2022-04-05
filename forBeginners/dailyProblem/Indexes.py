57ca1ac591f46360d7a7f11e27c20b78bf1cbf98
LoicBaillif <larmedeciel@gmail.com>
2022-04-05 19:48:56 +0200

    daily

---
+++ forBeginners/dailyProblem/Indexes.py
@@ -0,0 +1,7 @@
+# ****** PYTHON - Daily Problem : Indexes ******
+
+# Find the first letter of a person's name and print it out.
+# Make use of the variable name that stores a string.
+
+name = input()
+print(name[0])
