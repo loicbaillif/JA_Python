065533981cf027fedbe79fbb4a985b35e29d86f6
loicbaillif <larmedeciel@gmail.com>
2022-04-07 21:35:26 +0200

    Update overviewBasicProgram.py

---
+++ forBeginners/dailyProblem/overviewBasicProgram.py
@@ -0,0 +1,10 @@
+# ****** DAILY PROBLEM - Overview of a basic program ******
+# Write a program that prints the string "My name is " followed 
+# by your name like in the output example. All words in this string 
+# must be separated by a single space.
+
+# Output example
+# My name is John
+
+my_name = 'Mouarf'
+print('My name is ' + my_name)
