830af3f267b28766a1699d3cf2c8ab3a01aff582
loicbaillif <larmedeciel@gmail.com>
2022-04-08 21:06:51 +0200

    Daily Problem

---
+++ forBeginners/dailyProblem/differentAlphabets.py
@@ -0,0 +1,10 @@
+# ****** DAILY PROBLEM - Different Alphabets ******
+#
+# We have created a variable alphabet for lowercase alphabets of different 
+# languages. Your task is to print the 15th letter of this string.
+
+# don't modify the variable below, please
+alphabet = input()
+# put your python code here
+print(alphabet[14])
+print("abcdef"[2])
