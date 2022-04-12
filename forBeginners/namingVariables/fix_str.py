36c2d6532f52acc78901f0dd00e7fded7c1e05f6
LoicBaillif <larmedeciel@gmail.com>
2022-04-12 20:00:13 +0200

    Exercise
    Fix str

---
+++ forBeginners/namingVariables/fix_str.py
@@ -0,0 +1,17 @@
+# ****** PYTHON - Naming Variables : Fix str ******
+
+# The previous developer accidentally named a variable str. Since this name is 
+# reserved for a built-in type, now the code fails.
+
+# Please fix the variable name by adding an underscore, as suggested in the 
+# topic. The value of your new variable shouldn't change, i.e. it should look 
+# like Hello10. You don't have to print anything.
+
+# Remember that the str() function returns the string version of the given object.
+# *************************************************
+
+# str = "Hello"
+# str = str + str(10)
+
+str_ = "Hello"
+str_ = str_ + str(10)
