1271e39deb02dd9d531e8cd637fb96a163cda8de
loicbaillif <larmedeciel@gmail.com>
2022-04-13 21:05:20 +0200

    Exercise
    This is Python

---
+++ forBeginners/namingVariables/this_is_python.py
@@ -0,0 +1,12 @@
+# ****** PYTHON - Naming Variables : This is Python ******
+
+# Rename the variables in the code below according to the Python code 
+# style convention.
+
+# Exercise:
+# httpResponse = 'mocked response'
+# httpError = 404
+
+# Proposal
+http_response = 'mocked response'
+http_error = 404
