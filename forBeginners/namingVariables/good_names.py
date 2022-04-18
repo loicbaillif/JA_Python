7a77376c6ddacd0e41cb41dfbb3dee6461cfc3db
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-04-18 14:57:50 +0200

    Exercise
    Good names

---
+++ forBeginners/namingVariables/good_names.py
@@ -0,0 +1,17 @@
+# ****** NAMING VARIABLES - Exercise : Good names ******
+print("*"*60)
+print("* NAMING VARIABLES - Exercise : Good names")
+print("*"*60)
+# Here's a list of variables:
+#     - number_of_people_in_the_room = 10
+#     - model_score = 0.9875
+#     - client_name = "Bob"
+#     - colorOfTheShirt = "red"
+# Some of them are named according to naming conventions and best practices, 
+# others are not.
+# Copy only the lines with those variables that have good names in your code. 
+# You don't need to use the print() function.
+# ******************************************************
+
+model_score = 0.9875
+client_name = "Bob"
