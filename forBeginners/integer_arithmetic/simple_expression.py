09f0f20658a85a406fd9fda46367be70c57b38a8
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-04-22 22:41:21 +0200

    Exercise

---
+++ forBeginners/integer_arithmetic/simple_expression.py
@@ -0,0 +1,9 @@
+# Write a program that takes 3 integer numbers a, b and c, 
+# calculates a times b and then subtracts c from the product. 
+# The program should print the result.
+
+a = int(input("a please? "))
+b = int(input("b please? "))
+c = int(input("c please? "))
+
+print(a * b - c)  # No parenthesis needed
