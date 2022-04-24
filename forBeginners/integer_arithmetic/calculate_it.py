143731112881839a3c796762900fd558f17cb238
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-04-24 16:19:33 +0200

    Exercise "Calculate it"

---
+++ forBeginners/integer_arithmetic/calculate_it.py
@@ -0,0 +1,7 @@
+# ****** Python for Beginners - Basic data types and operations ******
+# ****** Eercise : Integer arithmetic ******
+
+# Print the result of executing the following expression: 
+# multiply 1234567890 by 987654321, then add 67890
+
+print(1234567890 * 987654321 + 67890)
