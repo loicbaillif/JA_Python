2fa88b67d9a1f06f444c04fcd4f6c18262da1f94
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-04-25 12:23:32 +0200

    Exercise
    Raise to power

---
+++ forBeginners/integer_arithmetic/raise_to_power.py
@@ -0,0 +1,8 @@
+# ****** Python for Beginners - Integer arithmetic ******
+# ****** Eercise : Raise to power ******
+#
+# It's time for really big numbers! Calculate this 2^{179} 
+# and print what you got
+ 
+result = 2**179
+print(result)
