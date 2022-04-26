bd6cee7907c0f0ea8fdbff43dd1e7a5f2cad1b4a
loicbaillif <larmedeciel@gmail.com>
2022-04-26 20:04:47 +0200

    Exercise
    Factorial 3

---
+++ forBeginners/integer_arithmetic/factorial_three.py
@@ -0,0 +1,7 @@
+# ****** Python for Beginners - Integer arithmetic ******
+# ****** Eercise : Factorial of 3 ******
+
+# Write a program that prints the product of these three numbers 1 * 2 * 3.
+
+a, b, c = 1, 2, 3
+print(a * b *  c)
