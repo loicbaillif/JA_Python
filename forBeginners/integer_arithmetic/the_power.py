96d8785ad8740daed5e01202201fd1013cddcb2a
loicbaillif <larmedeciel@gmail.com>
2022-04-27 20:53:35 +0200

    Exercise
    The power

---
+++ forBeginners/integer_arithmetic/the_power.py
@@ -0,0 +1,9 @@
+# ****** Python for Beginners - Integer arithmetic ******
+# ****** Eercise : the power ******
+
+# print() the result of the following expression: raise 31 to the power of 331 
+# and then get the remainder of the result's division by 20.
+
+a, b, c = 31, 331, 20
+
+print((a ** b) % 20)
