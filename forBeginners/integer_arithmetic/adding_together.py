8fcbc0c685bb86ec9e8ceacf92c58d1cfce815fa
loicbaillif <larmedeciel@gmail.com>
2022-04-29 20:52:01 +0200

    Subject

--- forBeginners/integer_arithmetic/adding_together.py
+++ forBeginners/integer_arithmetic/adding_together.py
@@ -1,3 +1,9 @@
+# ****** Python for Beginners - Integer arithmetic ******
+# ****** Exercise : Factorial of 3 ******
+
+# Write a program that takes two integer numbers a and b and prints their sum.
+# The variables have already been defined.
+
 a = int(input())
 b = int(input())
 
