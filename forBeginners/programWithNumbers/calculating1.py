96beb3db042163096c64f69378fd653db1e3ecad
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-04 20:15:44 +0200

    Final
    Exercise completed

---
+++ forBeginners/programWithNumbers/calculating1.py
@@ -0,0 +1,9 @@
+# ****** Python for Beginners - Program with numbers ******
+# ****** Exercise : Calculating an expression ******
+#
+# Write a program that reads an integer value n from the standard input and 
+# prints the result of the expression: 
+#       ((n + 1) * n + 2) * n + 3
+
+user_n = int(input())
+print(((user_n + 1) * user_n + 2) * user_n + 3)
