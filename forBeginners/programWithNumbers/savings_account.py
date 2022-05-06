65477605de54af5ccf7f363fee00ceb0ea62c40a
loicbaillif <larmedeciel@gmail.com>
2022-05-06 21:20:55 +0200

    Exercise
    Saving Accounts

---
+++ forBeginners/programWithNumbers/savings_account.py
@@ -0,0 +1,17 @@
+# ****** Python for Beginners - Program with numbers ******
+# ****** Exercise : Saving Accounts ******
+#
+# For the given amount of money, calculate the income from this 
+# savings account with a 5% interest rate after one year.
+# 
+# Here's the formula for calculating the income: 
+# income = (amount * interest * years) / 100 
+# 
+# Save the result into the variable income. You DO NOT need to print it.
+
+amount = 1000
+interest_rate = 5
+years = 1
+# change the next line
+to_percent = 100
+income = (amount * interest_rate * years) / to_percent
