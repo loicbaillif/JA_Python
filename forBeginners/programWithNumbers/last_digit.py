848abb585408dad9b39784b27c114e4d6abe74a3
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-07 11:40:11 +0200

    Final

--- forBeginners/programWithNumbers/last_digit.py
+++ forBeginners/programWithNumbers/last_digit.py
@@ -1,3 +1,7 @@
 # ****** Python for Beginners - Program with numbers ******
 # ****** Exercise : Last digit of a number ******
 #
+#  Write a program that reads an integer and outputs its last digit
+
+user_input = input()
+print(user_input[-1])
