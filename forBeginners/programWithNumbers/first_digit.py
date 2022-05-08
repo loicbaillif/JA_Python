895ea4afd0a8a87eb3753924cef4e44f4cd04274
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-08 10:09:17 +0200

    Exercise
    The first digit of a two-digit number

---
+++ forBeginners/programWithNumbers/first_digit.py
@@ -0,0 +1,7 @@
+# ****** Python for Beginners - Program with numbers ******
+# ****** Exercise : The first digit of a two-digit number ******
+
+# Write a program that takes a two-digit integer as an input 
+# and prints its first digit (i.e., the number of tens).
+
+print(input()[0])
