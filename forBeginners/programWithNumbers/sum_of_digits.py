85b45d4a369953ead4a188b3f18efc8149dc980d
loicbaillif <larmedeciel@gmail.com>
2022-05-09 20:35:09 +0200

    Exercise/Final
    Sum of digits

--- forBeginners/programWithNumbers/sum_of_digits.py
+++ forBeginners/programWithNumbers/sum_of_digits.py
@@ -8,7 +8,7 @@
 # (for example, you can get 8 from the number 508 by taking the remainder 
 # of the division by 10).
 
-user_input = int(input("Enter a three-digit integer/n"))
+user_input = int(input("Enter a three-digit integer\n"))
 units = user_input % 10
 tenths = (user_input // 10) % 10
 hundreds = user_input // 100
