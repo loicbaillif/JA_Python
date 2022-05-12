9042fcb638045fc3585924990381e602ba966b4c
loicbaillif <larmedeciel@gmail.com>
2022-05-12 20:50:54 +0200

    PyForBeg/ProgNum/Ex/Nuts/init
    How many nuts will be left after division?

---
+++ forBeginners/programWithNumbers/nuts.py
@@ -0,0 +1,10 @@
+# ****** Python for Beginners - Program with numbers ******
+# ****** Exercise : How many nuts will be left after division ******
+#
+# N squirrels found K nuts and decided to divide them equally. 
+# Find how many nuts will be left after each of the squirrels takes an 
+# equal number of nuts.
+#
+# Input data format 
+# There are two positive integers N and K, neither of them 
+# is greater than 10000.
