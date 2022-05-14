f4dd536d4a38b6ea4c4c3be6db08c2ed8a8a91dd
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-14 18:18:42 +0200

    PyForBeg/ProgWithNumbers/Ex/GoodRestVac/init
    Good rest on vacation exercise

---
+++ forBeginners/programWithNumbers/good_rest_on_vacation.py
@@ -0,0 +1,15 @@
+# Program with Numbers #
+# Write a program that will help people who are going on vacation. 
+# The program should calculate the total required sum (e.g. $) of money 
+# to have a good rest for a given duration.
+#
+# There are four parameters that have to be considered:
+#   . duration in days
+#   . total food cost per day
+#   . one-way flight cost 
+#   . cost of one night in a hotel (the number of nights is equal to the 
+#     duration of days minus one)
+#
+# Read integer values of these parameters from the standard input and 
+# then print the result.
+
