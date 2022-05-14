7ac2951e30dd919085df13dd51e8c1715fa0127d
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-14 18:12:56 +0200

    PyForBeg/Comparisons/inTheMiddle/final

--- forBeginners/comparisons/in_the_middle.py
+++ forBeginners/comparisons/in_the_middle.py
@@ -2,3 +2,5 @@
 # Write a program that reads an integer value from input and checks 
 # if it is less than 10 or greater than 250
 
+user_input = int(input())
+print(user_input < 10 or user_input > 250)
