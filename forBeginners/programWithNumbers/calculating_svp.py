fbd7d214b12fcd3ce10a6c856c57ff499cf4e3fe
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-17 20:00:27 +0200

    PyForBeg/ProgWithNumbers/Ex/CalcSVP/ft
    First run ... Valid

--- forBeginners/programWithNumbers/calculating_svp.py
+++ forBeginners/programWithNumbers/calculating_svp.py
@@ -3,3 +3,14 @@
 # (3 integers representing the length, width, and height) and print the sum 
 # of edge lengths, its total surface area, and the volume, one answer per line.
 
+length = int(input())
+width = int(input())
+height = int(input())
+
+sum_edge_lengths = 4 * (length + width + height)
+surface = 2 * (length * width + width * height + length * height)
+volume = length * width * height
+
+print(sum_edge_lengths)
+print(surface)
+print(volume)
