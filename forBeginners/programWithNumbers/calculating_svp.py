6151ecfd6db1ed72fcbafbf4a78991970755cef2
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-17 19:57:07 +0200

    PyForBeg/ProgWithNumbers/Ex/CalcSVP/init
    Calculating SVP

---
+++ forBeginners/programWithNumbers/calculating_svp.py
@@ -0,0 +1,5 @@
+# Program with Numbers - Calculating S V P #
+# Ask the user about parameters of a rectangular parallelepiped 
+# (3 integers representing the length, width, and height) and print the sum 
+# of edge lengths, its total surface area, and the volume, one answer per line.
+
