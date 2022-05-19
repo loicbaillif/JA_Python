041a570c8258f4597dc6fe1f900e6862e68d50fc
loicbaillif <larmedeciel@gmail.com>
2022-05-19 20:38:27 +0200

    PyForBeg/Comparisons/inTheMiddle/ft
    Create 2 variables to avoid magic number situation.

--- forBeginners/comparisons/in_the_middle.py
+++ forBeginners/comparisons/in_the_middle.py
@@ -2,5 +2,7 @@
 # Write a program that reads an integer value from input and checks 
 # if it is less than 10 or greater than 250
 
+low_limit = 10
+high_limit = 250
 user_input = int(input())
-print(user_input < 10 or user_input > 250)
+print(user_input < low_limit or user_input > high_limit)
