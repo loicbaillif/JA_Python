0c558f4030c9b9f2277e5e2cc929b58d9393317b
loicbaillif <larmedeciel@gmail.com>
2022-05-24 20:07:43 +0200

    PyForBeg/Comparisons/positive/init
    Exercise: Focus on the positive

---
+++ forBeginners/comparisons/positive.py
@@ -0,0 +1,9 @@
+# Basic Data Types and Operations #
+# Comparisons #
+# Write a program that reads an integer value from the input and checks 
+# if it is positive.
+#
+# Hints: 
+# 0 is not a positive number.
+# A comparison already returns a boolean, so if you need the result of the 
+# comparison, you can print it, like this print(5 > 9) # False
