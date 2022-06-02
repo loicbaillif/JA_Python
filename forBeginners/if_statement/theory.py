521c8f3822e916e1b9084e3849a079e125e5d9fd
loicbaillif <larmedeciel@gmail.com>
2022-06-02 20:54:50 +0200

    PyForBeg/If/theory/v2
    if block + instructino at end of block

--- forBeginners/if_statement/theory.py
+++ forBeginners/if_statement/theory.py
@@ -3,7 +3,10 @@
 
 # 1) Simple IF statement
 # Indentation is all we need to identify blocks:
-biscuits = 17
+biscuits = 1
 minimum_biscuits = 5
+print("Let's check stock...")
 if biscuits >= minimum_biscuits:
     print("Time for Tea")
+    print("With a cloud of milk?")
+print("See you tomorrow")
