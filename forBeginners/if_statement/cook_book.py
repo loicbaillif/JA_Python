1c681b90d86126f009484adc057f6fb4c3bbc830
loicbaillif <larmedeciel@gmail.com>
2022-06-02 21:25:05 +0200

    PyForBeg/If/ex1/init
    Cook Book exercise

---
+++ forBeginners/if_statement/cook_book.py
@@ -0,0 +1,19 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** IF statement ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" IF statement: Exercise - Cook book".center(80, "*"))
+print(separator.center(80)+"\n\n")
+
+# Any recipe starts with a list of ingredients. # Below is an extract from a 
+# cookbook with the ingredients for some dishes. Write a program that  
+# tells you what recipe you can make based on the ingredient you have.
+
+pasta = "tomato, basil, garlic, salt, pasta, olive oil"
+apple_pie = "apple, sugar, salt, cinnamon, flour, egg, butter"
+ratatouille = "aubergine, carrot, onion, tomato, garlic, olive oil, pepper, salt"
+chocolate_cake = "chocolate, sugar, salt, flour, coffee, butter"
+omelette = "egg, milk, bacon, tomato, salt, pepper"
+
+
