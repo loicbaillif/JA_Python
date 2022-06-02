7216a7868d321237bec9614520643049a2c9c40d
loicbaillif <larmedeciel@gmail.com>
2022-06-02 21:03:22 +0200

    PyForBeg/If/theory/v3
    Nested IF statement

--- forBeginners/if_statement/theory.py
+++ forBeginners/if_statement/theory.py
@@ -1,7 +1,10 @@
 #  ******** JetBrains Academy - Python for Beginners ********
 #  ****** IF statement ******
 
-# 1) Simple IF statement
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" IF statement ".center(80, "*"))
+print("_"*40+"\n\n")
+print(" 1) Simple IF statement ".center(60, "#"))
 # Indentation is all we need to identify blocks:
 biscuits = 1
 minimum_biscuits = 5
@@ -10,3 +13,14 @@ if biscuits >= minimum_biscuits:
     print("Time for Tea")
     print("With a cloud of milk?")
 print("See you tomorrow")
+
+print("_"*30+"\n")
+print(" 2) Nested IF statement ".center(60, "#"))
+rainbow = "red, orange, yellow, green, blue, indigo, violet"
+warm_colours = "red, yellow, orange"
+my_colour = "orange"
+if my_colour in rainbow:
+    print("Wow! your colour is in the rainbow")
+    if my_colour in warm_colours:
+        print("Oh by the way, it's a warm colour")
+    
