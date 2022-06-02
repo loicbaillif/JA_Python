6612291e9b7751445ecd19674b601a5eafd2e4df
loicbaillif <larmedeciel@gmail.com>
2022-06-02 20:33:39 +0200

    PyForBeg/If/theory/v1
    Theory initialization

---
+++ forBeginners/if_statement/theory.py
@@ -0,0 +1,9 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** IF statement ******
+
+# 1) Simple IF statement
+# Indentation is all we need to identify blocks:
+biscuits = 17
+minimum_biscuits = 5
+if biscuits >= minimum_biscuits:
+    print("Time for Tea")
