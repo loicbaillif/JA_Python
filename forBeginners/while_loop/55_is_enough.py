799956d9c0d04bf5fb4de868d00068d7126ca09b
loicbaillif <larmedeciel@gmail.com>
2022-06-03 21:28:00 +0200

    PyForBeg/While/ex1/init
    55 is enough

---
+++ forBeginners/while_loop/55_is_enough.py
@@ -0,0 +1,7 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** While Loop ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" While loop: Exercise - 55 is enough".center(80, "*"))
+print(separator.center(80)+"\n")
