6f462b3634aafc83fca1e9dc27b542f2a85bc3a8
loicbaillif <larmedeciel@gmail.com>
2022-06-03 17:54:44 +0200

    PyForBeg/Comparisons/ex3/v3
    Code optimized. Intermediate variables removed.

--- forBeginners/comparisons/guessing_game.py
+++ forBeginners/comparisons/guessing_game.py
@@ -4,10 +4,8 @@
 separator = "_"*40
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Comparisons: Exercise - Guessing game".center(80, "*"))
-print(separator.center(80)+"\n\n")
+print(separator.center(80)+"\n")
 
 set_number = 6557
 
-user_input1 = int(input())
-user_input2 = int(input())
-print(set_number == user_input1 * user_input2)
+print(set_number == int(input()) * int(input()))
