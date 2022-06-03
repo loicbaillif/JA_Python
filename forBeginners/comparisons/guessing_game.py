d4f7daa2b372433f0e00f6eca6f2635d035fe443
loicbaillif <larmedeciel@gmail.com>
2022-06-03 17:45:27 +0200

    PyForBeg/Comparisons/ex3/v1
    set_number

--- forBeginners/comparisons/guessing_game.py
+++ forBeginners/comparisons/guessing_game.py
@@ -5,3 +5,5 @@ separator = "_"*40
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Comparisons: Exercise - Guessing game".center(80, "*"))
 print(separator.center(80)+"\n\n")
+
+set_number = 6557
