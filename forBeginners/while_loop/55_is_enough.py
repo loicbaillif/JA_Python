a0a59b8b0055604d31952c826b2669306d39aaee
loicbaillif <larmedeciel@gmail.com>
2022-06-03 21:48:44 +0200

    PyForBeg/While/ex1/v1
    First run after some debugs

--- forBeginners/while_loop/55_is_enough.py
+++ forBeginners/while_loop/55_is_enough.py
@@ -5,3 +5,17 @@ separator = "_"*40
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" While loop: Exercise - 55 is enough".center(80, "*"))
 print(separator.center(80)+"\n")
+
+nb_inputs = 0
+sum_inputs = 0
+final_number = 55  # Avoid magic numbers
+user_input = int(input())  # Needed for first input
+
+while user_input != final_number:
+    nb_inputs += 1
+    sum_inputs += user_input    
+    user_input = int(input())
+
+print(nb_inputs)
+print(sum_inputs)
+print(round(sum_inputs / nb_inputs))
