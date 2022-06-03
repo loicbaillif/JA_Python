c8181de07e9b9670642fa8439a626e4886e58f96
loicbaillif <larmedeciel@gmail.com>
2022-06-03 17:52:51 +0200

    PyForBeg/Comparisons/ex3/v2
    Task complete. To be tuned.

--- forBeginners/comparisons/guessing_game.py
+++ forBeginners/comparisons/guessing_game.py
@@ -7,3 +7,7 @@ print(" Comparisons: Exercise - Guessing game".center(80, "*"))
 print(separator.center(80)+"\n\n")
 
 set_number = 6557
+
+user_input1 = int(input())
+user_input2 = int(input())
+print(set_number == user_input1 * user_input2)
