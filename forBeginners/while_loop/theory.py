c0d6c2d1461299ce9db6d757d77fee2a6dac76de
loicbaillif <larmedeciel@gmail.com>
2022-06-03 21:15:16 +0200

    PyForBeg/While/Theory/v1
    Section 1 : simple while loop

--- forBeginners/while_loop/theory.py
+++ forBeginners/while_loop/theory.py
@@ -4,3 +4,10 @@
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" While loop: Theory ".center(80, "*"))
 print("_"*40+"\n\n")
+print(" 1) Simple while loop ".center(60, "#"))
+number = 0
+while number < 5:
+    print(number)
+    number += 1
+print("Loop is complete, number = " + str(number))
+
