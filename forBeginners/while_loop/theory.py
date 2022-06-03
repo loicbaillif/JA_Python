61e967be89c172399b98473b93c8486a0df79572
loicbaillif <larmedeciel@gmail.com>
2022-06-03 21:11:58 +0200

    PyForBeg/While/Theory/Init
    While loop : Theory

---
+++ forBeginners/while_loop/theory.py
@@ -0,0 +1,6 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** WHILE loop ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" While loop: Theory ".center(80, "*"))
+print("_"*40+"\n\n")
