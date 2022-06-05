fc546a062fb86abcd44b46c4b527c991825da89c
loicbaillif <larmedeciel@gmail.com>
2022-06-05 08:34:55 +0200

    PyForBeg/If/ex3/v1
    First guess ... Test to come

--- forBeginners/if_statement/university.py
+++ forBeginners/if_statement/university.py
@@ -5,3 +5,21 @@ separator = "_"*40
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" IF statement: Exercise - University".center(80, "*"))
 print(separator.center(80)+"\n\n")
+
+harvard = "linguistics, physics, programming, fine arts"
+stanford = "biology, classics, geophysics, music"
+
+arts = "linguistics, fine arts, classics, music"
+sciences = "physics, programming, biology, geophysics"
+
+major = "..."  # What if major = "biology"?
+if major in harvard:
+    if major in arts:
+        print("This is an arts program at Harvard")
+    if major in sciences:
+        print("This is a sciences program at Harvard")
+if major in stanford:
+    if major in arts:
+        print("This is an arts program at Stanford")
+    if major in sciences:
+        print("This is a sciences program at Stanford")  # output
