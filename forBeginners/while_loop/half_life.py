7b6d815c483290c16170ce87988c090e1465d2a6
loicbaillif <larmedeciel@gmail.com>
2022-06-08 21:12:12 +0200

    PyForBeg/While/ex4/v2
    Input and output texts added.
    Final version.

--- forBeginners/while_loop/half_life.py
+++ forBeginners/while_loop/half_life.py
@@ -6,14 +6,14 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" While loop: Exercise - Half-life ".center(80, "*"))
 print(separator.center(80)+"\n")
 
-initial_quantity = int(input())
-final_quantity = int(input())
+initial_quantity = int(input("Please enter initial quantity of Radium:\n"))
+final_quantity = int(input("Please enter final quantity to reach:\n"))
 nb_half_life = 0
-days_per_half_life = 12
+days_per_half_life = 12  # avoid magic numbers ...
 
 while initial_quantity > final_quantity:
     nb_half_life += 1
     initial_quantity /= 2
 
-nb_days = nb_half_life * days_per_half_life
-print(nb_days)
+nb_days = nb_half_life * days_per_half_life  # Thank you Marie Curie :)
+print("You will reach that quantity in " + str(nb_days) + " days.")
