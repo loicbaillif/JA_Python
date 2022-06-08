d1a12a5bf9c402a1651a25b2ca950fb2283985f2
loicbaillif <larmedeciel@gmail.com>
2022-06-08 20:55:28 +0200

    PyForBeg/While/ex4/v1
    First version, validated with some tests.

--- forBeginners/while_loop/half_life.py
+++ forBeginners/while_loop/half_life.py
@@ -6,3 +6,14 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" While loop: Exercise - Half-life ".center(80, "*"))
 print(separator.center(80)+"\n")
 
+initial_quantity = int(input())
+final_quantity = int(input())
+nb_half_life = 0
+days_per_half_life = 12
+
+while initial_quantity > final_quantity:
+    nb_half_life += 1
+    initial_quantity /= 2
+
+nb_days = nb_half_life * days_per_half_life
+print(nb_days)
