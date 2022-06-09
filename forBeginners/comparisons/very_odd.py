a2d0ef0fb7a7b9a0e0025f7b1b9038a1eaa914be
loicbaillif <larmedeciel@gmail.com>
2022-06-09 20:36:47 +0200

    PyForBeg/Comparisons/ex5/init
    Very Odd : Initialization

---
+++ forBeginners/comparisons/very_odd.py
@@ -0,0 +1,7 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Comparisons ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Comparisons: Exercise - Very Odd ".center(80, "*"))
+print(separator.center(80)+"\n")
