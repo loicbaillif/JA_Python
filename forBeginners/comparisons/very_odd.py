d0466adc5c297216246a70a17427c5f9cd26309b
loicbaillif <larmedeciel@gmail.com>
2022-06-09 20:38:51 +0200

    PyForBeg/Comparisons/ex5/v1
    Variables initialization

--- forBeginners/comparisons/very_odd.py
+++ forBeginners/comparisons/very_odd.py
@@ -5,3 +5,15 @@ separator = "_"*40
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Comparisons: Exercise - Very Odd ".center(80, "*"))
 print(separator.center(80)+"\n")
+
+# Find out if the result of dividing A by B is an odd number.
+#
+# The input format:
+#   The first line is the dividend (A) and the second line is the divisor (B).
+#   It is guaranteed that the numbers are divided without remainder.
+#
+# The output format:
+#   True or False
+
+a_input = int(input())
+b_input = int(input())
