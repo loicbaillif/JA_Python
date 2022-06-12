86df788a44225729c9160622e74190e5e141f042
loicbaillif <larmedeciel@gmail.com>
2022-06-12 12:13:15 +0200

    PyForBeg/zookeeper/st4/v2
    Subject

--- forBeginners/chatty_bot/stage_1.py
+++ forBeginners/chatty_bot/stage_1.py
@@ -5,3 +5,13 @@ separator = "_"*40
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Chatty bot project: Stage 1 ".center(80, "*"))
 print(separator.center(80)+"\n")
+
+# For the first stage, you will write a bot who displays a greeting, 
+# its name, and the date of its creation. First impressions count!
+#
+# Your program should print the following lines:
+#
+#   Hello! My name is {bot_name}.
+#   I was created in {birth_year}.
+# Instead of {bot_name}, print any name you choose and replace {birth_year} 
+# with the current year (four digits).
