d10b9a1eaabf9c4495d78ec0fd69f4edcd0622ac
loicbaillif <larmedeciel@gmail.com>
2022-06-12 12:11:15 +0200

    PyForBeg/chatBot/stage1/init
    Chatty bot project: Stage 1

---
+++ forBeginners/chatty_bot/stage_1.py
@@ -0,0 +1,7 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Chatty bot project - Stage 1 ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Chatty bot project: Stage 1 ".center(80, "*"))
+print(separator.center(80)+"\n")
