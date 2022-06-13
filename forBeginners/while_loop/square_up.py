4eb9d29ee8804c1f2eda98ef12982cc9b5e82816
loicbaillif <larmedeciel@gmail.com>
2022-06-13 20:46:45 +0200

    PyForBeg/While/ex5/init
    While loop: Exercise
    Square up - fix the code : Initialization

---
+++ forBeginners/while_loop/square_up.py
@@ -0,0 +1,11 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** While Loop ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" While loop: Exercise - Square up ".center(80, "*"))
+print(separator.center(80)+"\n")
+
+# The program below is supposed to print the squares of all numbers 
+# from 1 to 20 (including), but there are some mistakes. Find and fix them.
+
