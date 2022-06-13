0d1254d90b33ec3f3aefdc7daca36fb4ea1460f2
loicbaillif <larmedeciel@gmail.com>
2022-06-13 20:47:40 +0200

    PyForBeg/While/ex5/v1
    Subject - Code to fix

--- forBeginners/while_loop/square_up.py
+++ forBeginners/while_loop/square_up.py
@@ -9,3 +9,6 @@ print(separator.center(80)+"\n")
 # The program below is supposed to print the squares of all numbers 
 # from 1 to 20 (including), but there are some mistakes. Find and fix them.
 
+i = 1
+while i > 20:
+    print(i)
