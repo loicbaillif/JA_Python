27c316da984ce9276baed4530a5e04b4551f08cd
loicbaillif <larmedeciel@gmail.com>
2022-06-16 20:35:54 +0200

    PyForBeg/While/ex6/v1
    First version, no test performed yet

--- forBeginners/while_loop/favor.py
+++ forBeginners/while_loop/favor.py
@@ -8,3 +8,10 @@ print(separator.center(80)+"\n")
 
 # Carl asks you to count the sum of the first k natural numbers. 
 # Read k from the input, then add up numbers from 1 to k and print your answer.
+
+k_input = int(input())
+sum = 0
+while k_input > 0:
+    sum += k_input
+    k_input -= 1
+print(sum)
