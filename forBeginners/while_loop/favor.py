271868b6eea5a22192fdfc0615479df6f1a9c558
loicbaillif <larmedeciel@gmail.com>
2022-06-16 20:32:25 +0200

    PyForBeg/While/ex6/init
    While loop: Exercise "Favor"
    File initialization
    Next: first code, no test

---
+++ forBeginners/while_loop/favor.py
@@ -0,0 +1,10 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** While Loop ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" While loop: Exercise - Favor ".center(80, "*"))
+print(separator.center(80)+"\n")
+
+# Carl asks you to count the sum of the first k natural numbers. 
+# Read k from the input, then add up numbers from 1 to k and print your answer.
