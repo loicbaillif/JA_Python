70f0713df27b4a7308aac87fb059692bab881412
loicbaillif <larmedeciel@gmail.com>
2022-06-17 20:27:18 +0200

    PyForBeg/If/ex4/init
    Exercise 4 : Plurals
    Initialization
    Next: Write first version for the program

---
+++ forBeginners/if_statement/plurals.py
@@ -0,0 +1,25 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** IF Statement ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" IF Statement: Exercise - Plurals ".center(80, "*"))
+print(separator.center(80)+"\n")
+
+# Alex wrote a program that reads a number and a word from the input to 
+# create phrases like "3 cats" and "1 dog". Unfortunately, the condition for 
+# plural nouns is currently missing. Alex doesn't know how to use if 
+# statements, but you do. Help Alex complete this program.
+# 
+# The plural form of a word generally ends with s. All numbers, apart from 1, 
+# expect the plural form after them, even zero: "0 birds".
+#
+# Words that form their plural not by adding s, will NOT appear in the tests.
+
+number = int(input())
+word = input()
+
+# write a condition for plurals
+
+
+print(number, word)
