0d6788dad296564056b0e7a130997713358ac51d
loicbaillif <larmedeciel@gmail.com>
2022-06-20 07:18:38 +0200

    PyForBeg/Comparisons/ex6/init
    Comparisons - Exercise 6 : Order!
    Initialization

---
+++ forBeginners/comparisons/order.py
@@ -0,0 +1,11 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Comparisons ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Comparisons: Exercise - Order! ".center(80, "*"))
+print(separator.center(80)+"\n")
+
+# Get three large numbers from input and check that they are given in 
+# ascending order. The subsequent number must be greater than the 
+# previous one.
