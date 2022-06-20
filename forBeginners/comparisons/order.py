b19108cae77ad5500d49d68f4310d1e2465522ee
loicbaillif <larmedeciel@gmail.com>
2022-06-20 07:20:31 +0200

    PyForBeg/Comparisons/ex6/v1
    First version
    Next: tests

--- forBeginners/comparisons/order.py
+++ forBeginners/comparisons/order.py
@@ -9,3 +9,9 @@ print(separator.center(80)+"\n")
 # Get three large numbers from input and check that they are given in 
 # ascending order. The subsequent number must be greater than the 
 # previous one.
+
+input1 = int(input())
+input2 = int(input())
+input3 = int(input())
+
+print(input2 > input1 and input3 > input2)
