bb397d8b936075e41219a443f5be974b5551e5f8
loicbaillif <larmedeciel@gmail.com>
2022-06-23 08:01:40 +0200

    PyForBeg/While/ex7/init
    While loop: Exercise - Factorial
    Initialization

---
+++ forBeginners/while_loop/factorial.py
@@ -0,0 +1,19 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** While loop ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" While loop: Exercise - Factorial ".center(80, "*"))
+print(separator.center(80)+"\n")
+
+# Factorial of the number N is the result of the multiplication of all 
+# positive integers less than or equal to N. For example, the factorial of 3 
+# is the product of 3, 2, 1, i.e. 3! = 3 x 2 x 1 = 6. Now, your task is to try 
+# and write a program that calculates the factorial of the input number 
+# using while loop.
+#
+#  The input format:
+#   The number N in range from 1 to 100.
+#
+# The output format:
+#   The factorial N!.
