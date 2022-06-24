286cfd05e3d2563ab02c5e6ba21ba9d57dda2b87
loicbaillif <larmedeciel@gmail.com>
2022-06-24 08:15:40 +0200

    PyForBeg/While/ex8/init
    While Loop - Rich Man's world exercise
    Initialization

---
+++ forBeginners/while_loop/rich.py
@@ -0,0 +1,23 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** While loop ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" While loop: Exercise - Rich Man's World ".center(80, "*"))
+print(separator.center(80)+"\n")
+
+
+# When a bank has financial problems the government can return a client's 
+# deposit if it is less than 700,000. The interest rate for a particular 
+# deposit is 7.1% a year. The percentages are paid to the same deposit at  
+# the end of the year and a new value of the interest is calculated.
+#
+# Find out how many years it will take for the sum of the deposit to exceed 
+# the value protected by the government.
+#
+# The input format:
+#   The initial sum of the deposit. It is guaranteed that the value 
+#   will be between 50,000 and 700,000.
+#
+# The output format:
+#   The number of years.
