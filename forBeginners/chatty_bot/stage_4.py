c57fc6615fd7628e61b871936f9d01049311f349
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-06-25 18:12:34 +0200

    PyForBeg/chatBot/stage4/init
    Chatty Bot - Stage 4 : Learning numbers
    Initialization

---
+++ forBeginners/chatty_bot/stage_4.py
@@ -0,0 +1,15 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Chatty bot project - Stage 4 ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Chatty bot project: Stage 4 ".center(80, "*"))
+print(separator.center(80)+"\n")
+
+# Description
+# Now you will teach your bot to count. It's going to become
+# an expert in numbers!
+#
+# Objective
+# At this stage, you will program the bot to count from 0 to any
+# positive number users enter.
