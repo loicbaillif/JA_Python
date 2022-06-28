aeb96499422d09bdf1d0be950fb543031fb100a0
loicbaillif <larmedeciel@gmail.com>
2022-06-28 07:16:20 +0200

    PyForBeg/InvokFunctions/theory/init
    Invoking functions: Theory

---
+++ forBeginners/functions/theory1.py
@@ -0,0 +1,6 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Invoking a function ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Invoking a function: Theory ".center(80, "*"))
+print("_"*40+"\n\n")
