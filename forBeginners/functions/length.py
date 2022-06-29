218360a34612b1aae33f62475e1debf6f6094131
loicbaillif <larmedeciel@gmail.com>
2022-06-29 21:44:55 +0200

    PyForBeg/InvokFunctions/ex2/v2
    Final version
    Code optimized
    Invite added
    Output phrase added

--- forBeginners/functions/length.py
+++ forBeginners/functions/length.py
@@ -5,6 +5,5 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Invoking a function: Exercise - String length ".center(80, "*"))
 print("_"*40+"\n\n")
 
-user_string = input()
-string_length = len(user_string)
-print(string_length)
+user_string = input("Please enter a string:\n> ")
+print(f"It is made of {len(user_string)} characters.")
