531822a2180e9317d23850ae718b617295e74951
loicbaillif <larmedeciel@gmail.com>
2022-06-29 21:32:51 +0200

    PyForBeg/InvokFunctions/ex1/v1
    First version
    Next: Add invite for user inputs

--- forBeginners/functions/young.py
+++ forBeginners/functions/young.py
@@ -4,3 +4,8 @@
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Invoking a function: Exercise - Young and Beautiful ".center(80, "*"))
 print("_"*40+"\n\n")
+
+jack_age = int(input())
+alex_age = int(input())
+lana_age = int(input())
+print(min(jack_age, alex_age, lana_age))
