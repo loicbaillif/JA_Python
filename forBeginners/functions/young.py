0151e0520687169ce3ce529a615651d3c9983f7f
loicbaillif <larmedeciel@gmail.com>
2022-06-29 21:25:20 +0200

    PyForBeg/InvokFunctions/ex1/init
    Functions exercise: Young & Beautiful
    Initialization
    Next: first proposal

---
+++ forBeginners/functions/young.py
@@ -0,0 +1,6 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Invoking a function ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Invoking a function: Exercise - Young and Beautiful ".center(80, "*"))
+print("_"*40+"\n\n")
