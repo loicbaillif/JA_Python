2c771200f1baa0dfe457e1a34591a60c38a4d177
loicbaillif <larmedeciel@gmail.com>
2022-06-29 07:15:27 +0200

    PyForBeg/InvokFunctions/theory/v1
    Using print() as example
    Next: Comment code for details

--- forBeginners/functions/theory1.py
+++ forBeginners/functions/theory1.py
@@ -4,3 +4,7 @@
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Invoking a function: Theory ".center(80, "*"))
 print("_"*40+"\n\n")
+print(" 1) Invoking print() ".center(60, "*"))
+print("Hello, World!")
+print()
+print("Bye,", "then!")
