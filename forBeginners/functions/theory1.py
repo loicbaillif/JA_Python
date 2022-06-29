7fdd5902c46924d50003e0cbed9afc57036985ff
loicbaillif <larmedeciel@gmail.com>
2022-06-29 20:56:01 +0200

    PyForBeg/InvokFunctions/theory/v4
    Built-in functions: first example
    Next: additional examples

--- forBeginners/functions/theory1.py
+++ forBeginners/functions/theory1.py
@@ -9,4 +9,8 @@ print("Hello, World!")      # Invoking print() with 1 argument
 print()                     # Invoking print() with no argument
 print("Bye,", "then!")      # Invoking print() with 2 arguments
 print("Lorem", "Ipsum", "Dolor", "Sit", "Amet", sep=".", end="\n\t")
-print("test")
+print("test\n")
+
+print(" 2) Built-in functions ".center(60, "*"))
+number = "1234"
+print(f"number = \"1234\"\nlen(number) = {len(number)}")
