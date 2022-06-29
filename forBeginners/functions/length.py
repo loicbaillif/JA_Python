e7dd0ae8494c6b4574b01beb0733b8b52549d4bc
loicbaillif <larmedeciel@gmail.com>
2022-06-29 21:41:34 +0200

    PyForBeg/InvokFunctions/ex2/v1
    First proposal
    Next: Add invite for user input, optimize code

--- forBeginners/functions/length.py
+++ forBeginners/functions/length.py
@@ -4,3 +4,7 @@
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Invoking a function: Exercise - String length ".center(80, "*"))
 print("_"*40+"\n\n")
+
+user_string = input()
+string_length = len(user_string)
+print(string_length)
