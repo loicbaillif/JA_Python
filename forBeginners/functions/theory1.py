693fa3bd2f77a19f522cbf4250db0929f32f32b4
loicbaillif <larmedeciel@gmail.com>
2022-06-29 07:18:36 +0200

    PyForBeg/InvokFunctions/theory/v2
    Comments added for details.
    Next: additional tests with print() based on official documentation

--- forBeginners/functions/theory1.py
+++ forBeginners/functions/theory1.py
@@ -5,6 +5,6 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Invoking a function: Theory ".center(80, "*"))
 print("_"*40+"\n\n")
 print(" 1) Invoking print() ".center(60, "*"))
-print("Hello, World!")
-print()
-print("Bye,", "then!")
+print("Hello, World!")      # Invoking print() with 1 argument
+print()                     # Invoking print() with no argument
+print("Bye,", "then!")      # Invoking print() with 2 arguments
