d3175b728897a30666a446e8468c8d6059602480
loicbaillif <larmedeciel@gmail.com>
2022-07-01 09:53:43 +0200

    PyForBeg/InvokFunctions/ex3/init
    Hello World revisited exercise
    Initialization

---
+++ forBeginners/functions/hello.py
@@ -0,0 +1,8 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Invoking a function ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Invoking a function: Exercise - Hello, World ".center(80, "*"))
+print("_"*40+"\n\n")
+
+name = input()
