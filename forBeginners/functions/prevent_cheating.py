559122f2cdceff279920b142693eff5b47859d92
loicbaillif <larmedeciel@gmail.com>
2022-07-02 16:28:45 +0200

    PyForBeg/InvokFunctions/ex4/v2
    First test: Successful
    Final version

--- forBeginners/functions/prevent_cheating.py
+++ forBeginners/functions/prevent_cheating.py
@@ -8,12 +8,13 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Declaring a function: Exercise - Prevent Cheating ".center(80, "*"))
 print(" " * 20 + "_" * 40 + " " * 20 +"\n\n")
 
+print(math.factorial(4))
 
 def new_math_factorial(number):
     print("Don't cheat!")
 
 math.factorial = new_math_factorial
 
-
+print(math.factorial(4))
 
 print(" " * 20 + "_" * 40 + " " * 20 +"\n\n")
