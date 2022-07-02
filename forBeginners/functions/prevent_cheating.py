aab4be83e3d5222b7b121abfcd277677c267ba63
loicbaillif <larmedeciel@gmail.com>
2022-07-02 16:14:54 +0200

    PyForBeg/InvokFunctions/ex4/init
    Prevent Cheating : Exercise
    Initialization

---
+++ forBeginners/functions/prevent_cheating.py
@@ -0,0 +1,10 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Declaring a function ******
+
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Declaring a function: Exercise - Prevent Cheating ".center(80, "*"))
+print(" " * 20 + "_" * 40 + " " * 20 +"\n\n")
+
+
+print(" " * 20 + "_" * 40 + " " * 20 +"\n\n")
