33f7806c082bb8457274219d6b1cad533a04bc4d
loicbaillif <larmedeciel@gmail.com>
2022-07-02 17:20:35 +0200

    PyForBeg/InvokFunctions/ex5/v1
    First implementation
    Next: Test it

--- forBeginners/functions/captain.py
+++ forBeginners/functions/captain.py
@@ -5,6 +5,7 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Declaring a function: Exercise - captain ".center(80, "*"))
 print(" " * 20 + "_" * 40 + " " * 20 +"\n\n")
 
-
+def captain_adder(name):
+    print(f"captain {name}")
 
 print(" " * 20 + "_" * 40 + " " * 20 +"\n\n")
