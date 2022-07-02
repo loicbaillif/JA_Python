37761ad6fedcfc8b213b93642f1d1ef65a09e590
loicbaillif <larmedeciel@gmail.com>
2022-07-02 15:26:37 +0200

    PyForBeg/InvokFunctions/theory2/v2
    First usage for multiply
    Next: define a method with no parameter

--- forBeginners/functions/theory2.py
+++ forBeginners/functions/theory2.py
@@ -1,6 +1,9 @@
 #  ******** JetBrains Academy - Python for Beginners ********
 #  ****** Declaring a function ******
 
+from audioop import mul
+
+
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Declaring a function: Theory ".center(80, "*"))
 print("_"*40+"\n\n")
@@ -8,3 +11,6 @@ print("_"*40+"\n\n")
 print(" First example ".center(60, "*"))
 def multiply(x, y):
     return x * y
+
+print(multiply(3, 4))
+print(multiply(6, 3))
