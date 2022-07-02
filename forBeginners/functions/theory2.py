145dddf6657e9e37408ca7b4523c28040ceee072
loicbaillif <larmedeciel@gmail.com>
2022-07-02 15:23:29 +0200

    PyForBeg/InvokFunctions/theory2/v1
    First example
    Next: use first function

--- forBeginners/functions/theory2.py
+++ forBeginners/functions/theory2.py
@@ -4,3 +4,7 @@
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Declaring a function: Theory ".center(80, "*"))
 print("_"*40+"\n\n")
+
+print(" First example ".center(60, "*"))
+def multiply(x, y):
+    return x * y
