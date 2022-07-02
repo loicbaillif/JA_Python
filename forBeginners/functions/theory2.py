db2018dfc87cbbf6800c83838175f803c755f67f
loicbaillif <larmedeciel@gmail.com>
2022-07-02 15:17:41 +0200

    PyForBeg/InvokFunctions/theory2/init
    Declaring a function: Theory
    Initialization

---
+++ forBeginners/functions/theory2.py
@@ -0,0 +1,6 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Declaring a function ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Declaring a function: Theory ".center(80, "*"))
+print("_"*40+"\n\n")
