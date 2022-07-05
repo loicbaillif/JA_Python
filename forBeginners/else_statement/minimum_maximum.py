4c0737f79b98a04ab1a1dffc553288f76c194318
loicbaillif <larmedeciel@gmail.com>
2022-07-05 10:26:30 +0200

    PyForBeg/else/ex1/init
    Exercise: Minimum Maximum
    Initialization

---
+++ forBeginners/else_statement/minimum_maximum.py
@@ -0,0 +1,7 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** else statement ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" else statement: Exercise - Minimum Maximum ".center(80, "*"))
+print("_"*40+"\n\n")
+
