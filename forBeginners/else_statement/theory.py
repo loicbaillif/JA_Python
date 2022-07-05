b3799adf51b06e2d2b41b281a92742fcc3752c2e
loicbaillif <larmedeciel@gmail.com>
2022-07-05 10:02:49 +0200

    PyForBeg/else/theory/v1
    First example for else statement

--- forBeginners/else_statement/theory.py
+++ forBeginners/else_statement/theory.py
@@ -5,6 +5,12 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" else statement: Theory ".center(80, "*"))
 print("_"*40+"\n\n")
 
-
+# First example:
+today = "working day"
+holiday = "holiday"
+if today == holiday:
+    print("Lucky you!")
+else:
+    print("Keep your chin up, then.")
 
 print("_"*40+"\n\n")
