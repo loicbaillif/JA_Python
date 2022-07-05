87023a5436188ac104183a1d1af6b96d003c27c0
loicbaillif <larmedeciel@gmail.com>
2022-07-05 10:34:45 +0200

    PyForBeg/else/ex1/v2
    Variables renamed for clarity
    Next: try with ternary operator

--- forBeginners/else_statement/minimum_maximum.py
+++ forBeginners/else_statement/minimum_maximum.py
@@ -5,10 +5,10 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" else statement: Exercise - Minimum Maximum ".center(80, "*"))
 print("_"*40+"\n\n")
 
-input1 = int(input())
-input2 = int(input())
+input_1 = int(input())
+input_2 = int(input())
 
-if input1 <= input2:
-    print(f"{input1} \n{input2}")
+if input_1 <= input_2:
+    print(f"{input_2} \n{input_1}")
 else:
-    print(f"{input2} \n{input1}")
+    print(f"{input_1} \n{input_2}")
