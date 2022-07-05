7d812957df9615bc5008e90bf8e06eb897a6a65d
loicbaillif <larmedeciel@gmail.com>
2022-07-05 09:59:34 +0200

    PyForBeg/else/theory/init
    Else statement: Theory
    Initialization

---
+++ forBeginners/else_statement/theory.py
@@ -0,0 +1,10 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** else statement ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" else statement: Theory ".center(80, "*"))
+print("_"*40+"\n\n")
+
+
+
+print("_"*40+"\n\n")
