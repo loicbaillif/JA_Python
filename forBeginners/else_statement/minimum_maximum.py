2bba4113742ace7a96c96981a8091ef36c2ce12c
loicbaillif <larmedeciel@gmail.com>
2022-07-05 10:31:42 +0200

    PyForBeg/else/ex1/v1
    First attempt

--- forBeginners/else_statement/minimum_maximum.py
+++ forBeginners/else_statement/minimum_maximum.py
@@ -5,3 +5,10 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" else statement: Exercise - Minimum Maximum ".center(80, "*"))
 print("_"*40+"\n\n")
 
+input1 = int(input())
+input2 = int(input())
+
+if input1 <= input2:
+    print(f"{input1} \n{input2}")
+else:
+    print(f"{input2} \n{input1}")
