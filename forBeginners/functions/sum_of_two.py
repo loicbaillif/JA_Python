dc06ce6a8a7beda1f3e25ab60b38a7470b9ef485
loicbaillif <larmedeciel@gmail.com>
2022-07-07 07:06:43 +0200

    PyForBeg/InvokFunctions/ex6/v1
    First version
    Next: run tests, correct if needed

--- forBeginners/functions/sum_of_two.py
+++ forBeginners/functions/sum_of_two.py
@@ -4,3 +4,7 @@
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Invoking a function - Exercise: Sum of two numbers ".center(80, "*"))
 print(("_"*40).center(80, " ") + "\n\n")
+
+x = int(input())
+y = int(input())
+print(sum(x, y))
