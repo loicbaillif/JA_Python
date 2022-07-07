dfe4a6086366a36e96beaa492db26bbe80225e23
loicbaillif <larmedeciel@gmail.com>
2022-07-07 07:05:53 +0200

    PyForBeg/InvokFunctions/ex6/init
    Invoking a function: Exercise - Sum of two
    Initialization

---
+++ forBeginners/functions/sum_of_two.py
@@ -0,0 +1,6 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Invoking a function ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Invoking a function - Exercise: Sum of two numbers ".center(80, "*"))
+print(("_"*40).center(80, " ") + "\n\n")
