9cf22c62e174ba5be4e0f3e2ac1ac80228131fad
loicbaillif <larmedeciel@gmail.com>
2022-07-11 08:09:56 +0200

    PyForBeg/InvokFunctions/ex7/v1
    This does not work, to be fixed.
    Next: first attempt

--- forBeginners/functions/make_it_work.py
+++ forBeginners/functions/make_it_work.py
@@ -4,3 +4,10 @@
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Declaring a function - Exercise: Make it work ".center(80, "*"))
 print(("_"*40).center(80, " ") + "\n\n")
+
+
+def closest_higher_mod_5(x):
+    remainder = x % 5
+    if remainder == 0:
+        return x
+    return "I don't know :("
