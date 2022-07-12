c71e9682cdf5239abd90da771f13fd5d314177fe
loicbaillif <larmedeciel@gmail.com>
2022-07-12 07:08:17 +0200

    PyForBeg/InvokFunctions/ex8/v1
    First implementation
    Next: Test it

--- forBeginners/functions/sum_of_two2.py
+++ forBeginners/functions/sum_of_two2.py
@@ -5,3 +5,5 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Invoking a function - Exercise: Sum of two ".center(80, "*"))
 print(("_"*40).center(80, " ") + "\n\n")
 
+def get_sum(a, b):
+    return a + b
