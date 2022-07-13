419d67a0bf2b89f9f85315f8a87d25cb6e84f431
loicbaillif <larmedeciel@gmail.com>
2022-07-13 07:05:14 +0200

    PyForBeg/InvokFunctions/ex9/v1
    First version
    Next: Test it

--- forBeginners/functions/miles_away.py
+++ forBeginners/functions/miles_away.py
@@ -4,3 +4,6 @@
 print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Declaring a function - Exercise: Miles away ".center(80, "*"))
 print(("_"*40).center(80, " ") + "\n\n")
+
+distance_miles = int(input())
+print(distance_miles * 1.609)
