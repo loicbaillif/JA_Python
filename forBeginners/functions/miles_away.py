71e6e6789030da549f24495438222fbf169ca2ca
loicbaillif <larmedeciel@gmail.com>
2022-07-13 07:07:20 +0200

    PyForBeg/InvokFunctions/ex9/v2
    Test OK, updated to create conversion variable
    Next: add invite in input, try to optimize code

--- forBeginners/functions/miles_away.py
+++ forBeginners/functions/miles_away.py
@@ -5,5 +5,6 @@ print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
 print(" Declaring a function - Exercise: Miles away ".center(80, "*"))
 print(("_"*40).center(80, " ") + "\n\n")
 
+km_per_mile = 1.609  # Create conversion variable to avoid magic number
 distance_miles = int(input())
-print(distance_miles * 1.609)
+print(distance_miles * km_per_mile)
