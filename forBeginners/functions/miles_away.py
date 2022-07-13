55c422a5d61ab33b797c3b4f999b4e522d20b5bc
loicbaillif <larmedeciel@gmail.com>
2022-07-13 07:04:09 +0200

    PyForBeg/InvokFunctions/ex9/init
    Invoking Functions - Exercise "Miles away"
    Initialization

---
+++ forBeginners/functions/miles_away.py
@@ -0,0 +1,6 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Declaring a function ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Declaring a function - Exercise: Miles away ".center(80, "*"))
+print(("_"*40).center(80, " ") + "\n\n")
