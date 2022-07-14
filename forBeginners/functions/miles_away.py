77fb1ae919fe0ec1480bbfc4236cf832d8d0b12c
loicbaillif <larmedeciel@gmail.com>
2022-07-14 18:12:42 +0200

    PyForBeg/InvokFunctions/ex9/v4
    Update:
    Created a dedicated function to convert from miles to kilometers, as requested in exercise
    Tested, validated.

--- forBeginners/functions/miles_away.py
+++ forBeginners/functions/miles_away.py
@@ -6,5 +6,9 @@ print(" Declaring a function - Exercise: Miles away ".center(80, "*"))
 print(("_"*40).center(80, " ") + "\n\n")
 
 km_per_mile = 1.609  # Create conversion variable to avoid magic number
+
+def miles_away(distance):
+    return distance * km_per_mile
+
 distance_miles = int(input("Please enter the distance in miles:\n> "))
-print(f"{distance_miles} miles = {km_per_mile * distance_miles} km.")
+print(f"{distance_miles} miles = {miles_away(distance_miles)} km.")
