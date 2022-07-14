57b462dec4b89ff834c862c84b67e9e838df9b70
loicbaillif <larmedeciel@gmail.com>
2022-07-14 19:37:44 +0200

    PyForBeg/else/ex5/init
    Exercise 5 : Opposite sign
    Initialization

---
+++ forBeginners/else_statement/opposite_sign.py
@@ -0,0 +1,9 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** else statement ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" else statement: Exercise - Opposite sign ".center(80, "*"))
+print("_"*40+"\n\n")
+
+# Read an integer as input and print it with the opposite sign.
+
