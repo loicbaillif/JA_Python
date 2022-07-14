dd466808187c6fde500e473451ba1a0fbde0d113
loicbaillif <larmedeciel@gmail.com>
2022-07-14 19:29:04 +0200

    PyForBeg/else/ex4/init
    Exercise 4: Shortcuts
    Initialization

---
+++ forBeginners/else_statement/shortcuts.py
@@ -0,0 +1,18 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** else statement ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" else statement: Exercise - Shortcuts ".center(80, "*"))
+print("_"*40+"\n\n")
+
+# Now let's rewrite this if-else statement. 
+# It should produce the same result but in one line.
+
+
+# Original:
+print("Have you had enough hours of sleep today?")
+answer = input()
+if answer == "yes":
+    print("Let's drink cocoa!")
+else:
+    print("I'd recommend a coffee!")
