0874ab41ec92d014ccf076eb56186c72edf530b0
loicbaillif <larmedeciel@gmail.com>
2022-07-14 19:05:37 +0200

    PyForBeg/else/ex3/init
    Exercise 3: Leap Year
    Initialization

---
+++ forBeginners/else_statement/leap_year.py
@@ -0,0 +1,13 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** else statement ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" else statement: Exercise - Healthy Sleep ".center(80, "*"))
+print("_"*40+"\n\n")
+
+# Write a program that checks if a year is leap.
+#
+# A year is considered leap if it is divisible by 4 and NOT divisible by 100, 
+# or if it is divisible by 400. So, 2000 is leap and 2100 isn't.
+#
+# Output either "Leap" or "Ordinary" depending on the input.
