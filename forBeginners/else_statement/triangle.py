1b4f25185be548056d3dc98643b7d4222efebdb2
loicbaillif <larmedeciel@gmail.com>
2022-07-15 16:45:09 +0200

    PyForBeg/else/ex6/init
    Exercise 6: Triangle or not triangle
    Initialization
    Next: First proposal

---
+++ forBeginners/else_statement/triangle.py
@@ -0,0 +1,10 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** else statement ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" else statement: Exercise - Triangle or not ".center(80, "*"))
+print("_"*40+"\n\n")
+
+# Read three angles given on separate input lines and check whether 
+# they form a triangle. Print the answer in the following format: 
+#   "The triangle is valid!" or "The triangle is not valid!".
