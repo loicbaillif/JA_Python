df1dbaf1445fd6d99e2a724364d4087c4b392264
loicbaillif <larmedeciel@gmail.com>
2022-07-16 14:31:28 +0200

    PyForBeg/loanCalc/init
    Loan Calculator: New project
    Project initialization
    Next: Stage 1

---
+++ forBeginners/loan_calculator/stage1.py
@@ -0,0 +1,7 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Loan calculator project - Stage 1 ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Loan calculator project: Stage 1 ".center(80, "*"))
+print(separator.center(80)+"\n")
