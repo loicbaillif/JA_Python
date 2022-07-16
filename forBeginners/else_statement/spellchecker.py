d7ef92ca28d4ea1033b0c1cc52921e45cfbabfc9
loicbaillif <larmedeciel@gmail.com>
2022-07-16 08:51:48 +0200

    PyForBeg/else/ex7/init
    Else Statement - Exercise 7
    Initialization

---
+++ forBeginners/else_statement/spellchecker.py
@@ -0,0 +1,13 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** else statement - Exercise ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" else statement: Exercise - Spellchecker ".center(80, "*"))
+print("_"*40+"\n\n")
+
+# Write a simple spellchecker that tells you if the word is spelled correctly. 
+# Use the dictionary in the code below: it contains the list of 
+# all correctly written words.
+
+dictionary = ["aa", "abab", "aac", "ba", "bac", "baba", "cac", "caac"]
+
