63f546320508fef3566d09ec20eda8a8fb7d6417
loicbaillif <larmedeciel@gmail.com>
2022-07-16 12:43:30 +0200

    PyForBeg/scope/ex2/init
    Exercise 2: hero Damage
    Initialization

---
+++ forBeginners/scope/hero_damage.py
@@ -0,0 +1,6 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Scope: Exercise - Cities ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Scope: Exercise - Hero Damage ".center(80, "*"))
+print("_"*40+"\n\n")
