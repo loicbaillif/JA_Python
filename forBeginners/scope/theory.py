293f0a00901318c7b137ac5d7eacf951bac1419f
loicbaillif <larmedeciel@gmail.com>
2022-07-16 09:08:17 +0200

    PyForBeg/scope/theory/init
    New chapter: Scope
    Theory
    File initialization

---
+++ forBeginners/scope/theory.py
@@ -0,0 +1,6 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Scope: Theory ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Scope: Theory ".center(80, "*"))
+print("_"*40+"\n\n")
