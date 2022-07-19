7161eebdbf880955ffba07a89ad2781464dd02b6
loicbaillif <larmedeciel@gmail.com>
2022-07-19 23:35:30 +0200

    PyForBeg/loadMod/theory/init
    Load Modules in Python: Theory
    Initialization

---
+++ forBeginners/load_module/theory.py
@@ -0,0 +1,7 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Load Module: Theory ******
+
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Load Module: Theory ".center(80, "*"))
+print("_"*40+"\n\n")
+
