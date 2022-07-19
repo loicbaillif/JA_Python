402178752bcf5e873e68ef1edd599cdc580eadf6
loicbaillif <larmedeciel@gmail.com>
2022-07-19 23:58:06 +0200

    PyForBeg/loadMod/theory/v6
    print_subtitle created

--- forBeginners/load_module/print_title.py
+++ forBeginners/load_module/print_title.py
@@ -4,3 +4,6 @@ def print_title(title):
     print("*"*80)
     print("* " + title.center(76, " ") + " *")
     print("*"*80 + "\n")
+
+def print_subtitle(subtitle):
+    print("\n" + "*"*10 + subtitle.center(40, " ") + "*"*10)
