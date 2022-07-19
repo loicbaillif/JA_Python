e99983f0091900a4a69c83f65c72c323d190cd56
loicbaillif <larmedeciel@gmail.com>
2022-07-20 00:35:56 +0200

    PyForBeg/loadMod/ex1/init
    Exercise 1 : Copysign
    Initialization
    Next: First proposal

---
+++ forBeginners/load_module/copysign.py
@@ -0,0 +1,16 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Load Module: Exercise - Copysign ******
+
+# Write a program that takes two float numbers, x and y, and prints x with 
+# the sign of y. Use the copysign function defined in the math module.
+
+
+# Standard Library imports
+
+# Local application imports
+from print_title import print_title
+from print_title import print_subtitle
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Load Module - Exercise")
+print_subtitle("Copysign")
