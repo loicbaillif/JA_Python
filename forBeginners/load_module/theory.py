252a5770faa8f9a55632dda9d241eb5d82813789
loicbaillif <larmedeciel@gmail.com>
2022-07-19 23:54:02 +0200

    PyForBeg/loadMod/theory/v5
    Import order: recommandation

--- forBeginners/load_module/theory.py
+++ forBeginners/load_module/theory.py
@@ -5,3 +5,10 @@ from print_title import print_title
 
 print_title("JetBrains Academy - Python for Beginners")
 print_title("Load Module")
+
+print("Recommended import order:")
+print("\t1. Standard library imports")
+print("\t2. Third party dependency import")
+print("\t3. Local application imports")
+print("Group them by category, and leave an empty line between 2 categories")
+
