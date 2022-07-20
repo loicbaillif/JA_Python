909619e4991f4d9c54539ff0079311ccb9311eeb
loicbaillif <larmedeciel@gmail.com>
2022-07-20 21:19:17 +0200

    PyForBeg/elif/theory/v1
    First example: if ... elif ... else
    Next: second example

--- forBeginners/elif_statement/theory.py
+++ forBeginners/elif_statement/theory.py
@@ -7,3 +7,12 @@ from print_title import print_subtitle
 
 print_title("JetBrains Academy - Python for Beginners")
 print_title("elif statement")
+
+print_subtitle("First example")
+price = 10000  # some int value
+if price > 5000:
+    print("This is too expansive!")
+elif price > 500:
+    print("I can afford that.")
+else:
+    print("Not expansive enough, my son!")
