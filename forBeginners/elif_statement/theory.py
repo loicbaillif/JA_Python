08a2cb53116ac423f5d36155673d864f739f4c11
loicbaillif <larmedeciel@gmail.com>
2022-07-20 21:10:37 +0200

    PyForBeg/elif/theory/init
    elif Statement: Theory
    Initialization

---
+++ forBeginners/elif_statement/theory.py
@@ -0,0 +1,9 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Theory ******
+
+# Local application imports
+from print_title import print_title
+from print_title import print_subtitle
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif statement")
