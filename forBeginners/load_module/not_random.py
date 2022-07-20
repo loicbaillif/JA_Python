a0cb4da38bb4435f881e1bba932ecda7c3db61ad
loicbaillif <larmedeciel@gmail.com>
2022-07-20 07:18:18 +0200

    PyForBeg/loadMod/ex2/init
    Load Modules - Exercise : Not exactly random
    Initialization

---
+++ forBeginners/load_module/not_random.py
@@ -0,0 +1,15 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Load Module: Exercise - Copysign ******
+
+
+# Python can generate random numbers using the random module. To do that, 
+# it takes one number as a starting point (it's called seed) and then uses 
+# a formula. By default, the current system time is used as seed. 
+# However, we can set the seed manually and thus make sure that our "random" 
+# number is always the same regardless of how many times we run the program.
+#
+# Write a program that takes an integer number n, sets the seed to that 
+# number, and then prints a random number from -100 to 100.
+# 
+# Use the seed and randint functions from the random module. The former takes 
+# one number and the latter takes two numbers that represent the range.
