62fba1905d1944a3aa099346dae77b9c37148422
loicbaillif <larmedeciel@gmail.com>
2022-07-20 21:01:26 +0200

    PyForBeg/loadMod/ex3/v1
    First proposal: To be tested

--- forBeginners/load_module/capitalize.py
+++ forBeginners/load_module/capitalize.py
@@ -4,9 +4,16 @@
 # Write a program that takes a string, capitalizes all words in it and then 
 # prints the result. Use the capwords function from the string module.
 
+
+# Standard Library imports
+from string import capwords
+ 
 # Local application imports
 from print_title import print_title
 from print_title import print_subtitle
 
 print_title("Jet Brains Academy - Python for Beginners")
 print_subtitle("Load Module: Exercise - Capitalize All Words")
+
+input_string = input()
+print(capwords(input_string))
