f883ff083d8e73038a44fd50ca5024060f92bae1
loicbaillif <larmedeciel@gmail.com>
2022-07-21 08:16:56 +0200

    PyForBeg/elif/ex1/init
    elif - Exercise: Army of units
    Initialization

---
+++ forBeginners/elif_statement/army.py
@@ -0,0 +1,20 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Exercise - The army of units ******
+
+# In a computer game, each gamer has an army of units.
+# Write a program that will classify the army of your enemies corresponding to the following rules:
+#   Units: Category
+#       . less than 1: no army
+#       . from 1 to 9: few
+#       . from 10 to 49: pack
+#       . from 50 to 499: horde
+#       . from 500 to 999: swarm
+#       . 1000 and more: legion
+# The program should read the number of units and output the corresponding category.
+
+# Local application imports
+from print_title import print_title
+from print_title import print_subtitle
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif statement: Exercise - The army of units")
