3b90a9b72b42fc497af0053b131cf4b37ebfedfd
loicbaillif <larmedeciel@gmail.com>
2022-07-27 05:46:01 +0200

    PyForBeg/elif/ex2/init
    Exercise Negative, Positive or Zero
    Initialization

---
+++ forBeginners/elif_statement/neg_pos_zero.py
@@ -0,0 +1,15 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Exercise - Negative, Positive or Zero ******
+
+# Write a program that reads an integer number and prints:
+#   "negative" if the number is less than 0;
+#   "positive" if the number is greater than 0;
+#   "zero" if the number equals 0.
+
+
+# Local application imports
+from print_title import print_title
+from print_title import print_subtitle
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif statement: Exercise - Negative, Positive or Zero")
