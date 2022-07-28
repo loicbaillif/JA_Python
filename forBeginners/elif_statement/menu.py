c2ffd5abd15a609d1df304d4da8d74df87ab86d3
loicbaillif <larmedeciel@gmail.com>
2022-07-28 20:20:16 +0200

    PyForBeg/elif/ex3/v1
    First proposal, to be tested

--- forBeginners/elif_statement/menu.py
+++ forBeginners/elif_statement/menu.py
@@ -16,3 +16,18 @@ from print_title import print_title
 
 print_title("JetBrains Academy - Python for Beginners")
 print_title("elif statement: Exercise - Menu")
+
+pizza = "Margherita, Four Seasons, Neapolitan, Vegetarian, Spicy"
+salad = "Caesar salad, Green salad, Tuna salad, Fruit salad"
+soup = "Chicken soup, Ramen, Tomato soup, Mushroom cream soup"
+not_on_menu = "Sorry, we don't have it on the menu"
+
+visitor_order = input()
+if visitor_order == "pizza":
+    print(pizza)
+elif visitor_order == "salad":
+    print(salad)
+elif visitor_order == "soup":
+    print(soup)
+else:
+    print(not_on_menu)
