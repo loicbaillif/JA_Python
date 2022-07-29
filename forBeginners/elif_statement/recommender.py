8830fec7fe1b9e6ce30a8b47c5f55b5484b2a170
loicbaillif <larmedeciel@gmail.com>
2022-07-29 21:45:02 +0200

    PyForBeg/elif/ex4/init
    Exercice: Recommender
    File initialization

---
+++ forBeginners/elif_statement/recommender.py
@@ -0,0 +1,23 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Exercise - Recommender ******
+
+# Write a program that recommends one of the following movies based on the age of a user:
+
+#     <=16: "Lion King"
+
+#     17 - 25: "Trainspotting"
+
+#     26 - 40: "Matrix"
+
+#     41−60: "Pulp Fiction"
+
+#     >60: "Breakfast at Tiffany's"
+
+# The user enters their age and the program outputs one title.
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif statement: Exercise - Recommender")
