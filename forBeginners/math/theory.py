d19e21258eebde5728b5041388e9dfb45db08b71
loicbaillif <larmedeciel@gmail.com>
2022-07-30 21:51:12 +0200

    PyForBeg/math/theory/v1
    First functions:
    abs(), round(), pow();
    Next: min() and max()

--- forBeginners/math/theory.py
+++ forBeginners/math/theory.py
@@ -8,4 +8,20 @@ from print_title import print_subtitle
 print_title("JetBrains Academy - Python for Beginners")
 print_title("Math functions")
 
+print_subtitle("Advanced Arithmetics")
 
+integer1 = -32
+float1 = -17.13579
+
+print("***** Absolute Value: abs(x)")
+print("abs(", integer1, ") = ", abs(integer1), sep='')
+print("abs(", float1, ") = ", abs(float1), sep='')
+
+print("\n***** Round: round(x)")
+print("round(", float1, ") = ", round(float1), sep='')
+print("round(", float1, ", 2) = ", round(float1, 2), sep='')
+print("round(", float1, ", 4) = ", round(float1, 4), sep='')
+
+print("\n***** Power: pow(x, y)")
+print("pow(2, 10) =", pow(2, 10))
+print("pow(2.0, 10) =", pow(2.0, 10))
