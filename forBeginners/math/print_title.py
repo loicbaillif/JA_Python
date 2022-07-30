12b4b2bfc5d3194df0ddc7177909501395e9081a
loicbaillif <larmedeciel@gmail.com>
2022-07-30 21:36:46 +0200

    PyForBeg/math/theory/init
    Math functions: Theory
    Initialization

---
+++ forBeginners/math/print_title.py
@@ -0,0 +1,9 @@
+# ***** THEORY MODULE ***** #
+
+def print_title(title):
+    print("*"*80)
+    print("* " + title.center(76, " ") + " *")
+    print("*"*80 + "\n")
+
+def print_subtitle(subtitle):
+    print("\n" + "*"*10 + subtitle.center(40, " ") + "*"*10)
---
+++ forBeginners/math/theory.py
@@ -0,0 +1,11 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Theory ******
+
+# Local application imports
+from print_title import print_title
+from print_title import print_subtitle
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions")
+
+
