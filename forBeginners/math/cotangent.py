83bef647bc7e426193e2daa098c5ac7a5d1464ce
loicbaillif <larmedeciel@gmail.com>
2022-07-31 22:01:05 +0200

    PyForBeg/math/ex3/init
    Exercise: Cotangent
    Initialization

---
+++ forBeginners/math/cotangent.py
@@ -0,0 +1,19 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - Cotangent ******
+
+# Write a program that reads an integer representing an angle (in degrees) 
+# and prints its cotangent.
+# Round the result to 10 decimal places.
+
+
+# Global Python import
+from math import sin, cos
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions - Exercise: Cotangent")
+
+
+print_title("End of exercise")
