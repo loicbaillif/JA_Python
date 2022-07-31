7e4d3ee3f107622ee39f59499e66ba9a9fb93bde
loicbaillif <larmedeciel@gmail.com>
2022-07-31 18:09:26 +0200

    PyForBeg/math/ex1/v1
    First version
    Next: improve program by removing temp variable s

--- forBeginners/math/heron.py
+++ forBeginners/math/heron.py
@@ -10,7 +10,12 @@ from print_title import print_title
 print_title("JetBrains Academy - Python for Beginners")
 print_title("Math functions - Exercise: Heron's Formula")
 
-
+a = int(input())
+b = int(input())
+c = int(input())
+p = (a + b + c) / 2
+s = math.sqrt(p * (p - a) * (p - b) * (p - c))
+print(s)
 
 
 print_title("End of exercise")
