66f64688453d66bf4825aa3276e63f5a47c870be
loicbaillif <larmedeciel@gmail.com>
2022-07-31 18:06:17 +0200

    PyForBeg/math/ex1/init
    Exercise 1: Heron's formula
    Initialization

---
+++ forBeginners/math/heron.py
@@ -0,0 +1,16 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - Heron's formula ******
+
+# Global Python import
+import math
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions - Exercise: Heron's Formula")
+
+
+
+
+print_title("End of exercise")
