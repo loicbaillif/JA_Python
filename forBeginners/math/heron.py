cdd4c58b1b0fd57936b630d51389d0cc0adc2a3f
loicbaillif <larmedeciel@gmail.com>
2022-07-31 18:11:45 +0200

    PyForBeg/math/ex1/v2
    temp variable removed
    Next: Test solution, and add invite for user inputs

--- forBeginners/math/heron.py
+++ forBeginners/math/heron.py
@@ -14,8 +14,7 @@ a = int(input())
 b = int(input())
 c = int(input())
 p = (a + b + c) / 2
-s = math.sqrt(p * (p - a) * (p - b) * (p - c))
-print(s)
+print(math.sqrt(p * (p - a) * (p - b) * (p - c)))
 
 
 print_title("End of exercise")
