e4bd0865deca36f88335ad36db67b6192d3087d3
loicbaillif <larmedeciel@gmail.com>
2022-07-31 21:50:03 +0200

    PyForBeg/math/ex2/init
    Exercise: Sine and Cosine
    Initialization

---
+++ forBeginners/math/sine_cosine.py
@@ -0,0 +1,20 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - Sine and Cosine ******
+
+# Write a program that reads a value representing an angle (in radians), 
+# and prints the difference between its sine and cosine.
+# Do not round the result.
+
+
+# Global Python import
+import math
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions - Exercise: Sine and Cosine")
+
+
+
+print_title("End of exercise")
