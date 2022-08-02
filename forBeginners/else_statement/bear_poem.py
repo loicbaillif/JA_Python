572c3d42ce6e00f8831be7b2e3699ab2cfbe075a
loicbaillif <larmedeciel@gmail.com>
2022-08-02 09:46:41 +0200

    PyForBeg/else/ex8/init
    Exercise: The bear poem
    Initialization

---
+++ forBeginners/else_statement/bear_poem.py
@@ -0,0 +1,23 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - The bear poem ******
+
+# When you were a child, you must have loved poems for kids. Below is one 
+# of them, its idea is the following: one bear thinks over what to do, then 
+# comes another one and they think together. Then comes the third one, the 
+# fourth one, etc. Here's the beginning of the poem:
+#   One little bear
+#   Wondering what to do
+#   Along came another
+#   Then there were two!
+#
+# Create a code that print the poem several times (user input), just changing
+# the number of bears
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Else Statement - Exercise: The bear poem")
+
+
+print_title("End of exercise")
