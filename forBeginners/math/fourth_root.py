0210288fad9f422eea9726bac72a5312387c0b7b
loicbaillif <larmedeciel@gmail.com>
2022-08-03 20:34:55 +0200

    PyForBeg/math/ex4/init
    Exercise 4: the fourth root
    Initialization

---
+++ forBeginners/math/fourth_root.py
@@ -0,0 +1,20 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - The fourth root ******
+
+# Write a program that prints the fourth root from a given real number.
+#
+# The square root function is located in the math module. 
+# Try to find and apply it.
+#
+#  Do not round the result.
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions - Exercise: The fourth root")
+
+
+
+
+print_title("End of exercise")
