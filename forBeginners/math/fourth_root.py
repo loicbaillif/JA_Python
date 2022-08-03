c71bcf362520dc319c7c3d7a6b8392aee0cd3831
loicbaillif <larmedeciel@gmail.com>
2022-08-03 20:40:52 +0200

    PyForBeg/math/ex4/v1
    First proposal
    Next: validate it, add invite for user input, set a dedicated function

--- forBeginners/math/fourth_root.py
+++ forBeginners/math/fourth_root.py
@@ -8,13 +8,18 @@
 #
 #  Do not round the result.
 
+
+# Global Python import
+from math import sqrt
+
 # Local application imports
 from print_title import print_title
 
 print_title("JetBrains Academy - Python for Beginners")
 print_title("Math functions - Exercise: The fourth root")
 
-
+user_number = float(input())
+print(sqrt(sqrt(user_number)))
 
 
 print_title("End of exercise")
