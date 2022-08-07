1f3987abd52bb6d177a36a9654e267629baeedef
loicbaillif <larmedeciel@gmail.com>
2022-08-07 10:32:41 +0200

    PyForBeg/math/ex5/init
    Exercise 5: Calculating the factorial
    Initialization

---
+++ forBeginners/math/factorial.py
@@ -0,0 +1,18 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - Calculating the factorial ******
+
+# Global Python import
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions - Exercise: Calculating the factorial")
+
+# Import the factorial function from the math module  
+# and print the value of the factorial of x.
+
+
+
+
+print_title("End of exercise")
