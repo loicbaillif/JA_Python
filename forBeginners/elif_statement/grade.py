e0039569fe6b0f4c9470ef307299be67f93f3cce
loicbaillif <larmedeciel@gmail.com>
2022-08-09 11:11:49 +0200

    PyForBeg/elif/ex6/init
    Exercise 6: Grade
    Initialization

---
+++ forBeginners/elif_statement/grade.py
@@ -0,0 +1,29 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Exercise - The farm ******
+
+# 
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif statement: Exercise - Grade")
+
+# There are a number of grades you can get in a test: A, B, C, D, F. 
+# The percentages are as follows:
+#   A: 90-100%
+#   B: 80-90%
+#   C: 70-80%
+#   D: 60-70%
+#   F: <60%
+# 
+# Determine the grade that a student will get based on the student's 
+# score and the maximum score.
+#
+# Note that the upper limit is not included in the range, except for the A
+# grade. For example, a student with 60% will get D, with 70% or 79.9% — C, 
+# but the top score 100% is just A.
+
+
+
+print_title("End of exercise")
