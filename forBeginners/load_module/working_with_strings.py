9be6c5d9881ff1eb023f945bfdd761d9e34205a5
loicbaillif <larmedeciel@gmail.com>
2022-08-10 08:57:55 +0200

    PyForBeg/loadMod/ex4/init
    Exercise 4: Working with strings
    Initialization

---
+++ forBeginners/load_module/working_with_strings.py
@@ -0,0 +1,20 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Load Modules: Exercise - Working with strings ******
+
+ 
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Load Modules: Exercise - Working with strings")
+
+# In the string module, there are constants digits and ascii_lowercase. 
+# Print digits in the first line of the output, 
+# and ascii_lowercase # in the second line.
+
+
+
+
+
+print_title("End of exercise")
