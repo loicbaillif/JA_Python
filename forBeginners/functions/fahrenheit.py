271a7be89f4049e79b52d5f79bec53a83b611722
loicbaillif <larmedeciel@gmail.com>
2022-08-11 09:17:31 +0200

    PyForBeg/declareFunc/ex6/init
    Declaring Functions - Exercise: Fahrenheit
    Initialization

---
+++ forBeginners/functions/fahrenheit.py
@@ -0,0 +1,20 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Declaring a function: Exercise - Fahrenheit ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Declaring a function: Exercise - Fahrenheit")
+
+# Convert the temperature from Fahrenheit to Celsius in the function below. 
+# You can use this formula:
+#
+#       C° = (F° - 32) * 5 / 9 
+# 
+# Round the returned result to 3 decimal places.
+
+
+
+print_title("End of exercise")
