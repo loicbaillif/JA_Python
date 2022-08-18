9675aa54f3cd964b4b40868336ae767b8da7ca64
loicbaillif <larmedeciel@gmail.com>
2022-08-18 18:22:59 +0200

    PyForBeg/InvokFunctions/ex10/v1
    First proposal: Operational but probably room for improvement.

--- forBeginners/functions/longest_word.py
+++ forBeginners/functions/longest_word.py
@@ -9,8 +9,14 @@ print_title("JetBrains Academy - Python for Beginners")
 print_title("Invoking a function: Exercise - Longest word")
 
 # Find the longest word in a pair and print its length.
-# The variables word1 and word2 are defined for you.
 
+word1 = input()
+word2 = input()
+
+if len(word1) >= len(word2):
+    print(len(word1))
+else:
+    print(len(word2))
 
 
 print_title("End of exercise")
