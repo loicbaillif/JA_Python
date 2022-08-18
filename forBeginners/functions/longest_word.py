e70832b953c2746b1b1ccece441eec259c06e9e9
loicbaillif <larmedeciel@gmail.com>
2022-08-18 18:16:43 +0200

    PyForBeg/InvokFunctions/ex10/init
    Invoking a function - Exercise: Longest word
    Initialization

---
+++ forBeginners/functions/longest_word.py
@@ -0,0 +1,18 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Invoking a function: Exercise - Longest word ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Invoking a function: Exercise - Longest word")
+
+# Find the longest word in a pair and print its length.
+# The variables word1 and word2 are defined for you.
+
+
+
+print_title("End of exercise")
+
+
