fc80cc64f5adf8e148a43b4017da651685501261
loicbaillif <larmedeciel@gmail.com>
2022-08-19 10:01:52 +0200

    PyForBeg/elif/ex8/init
    Exercise: What day is it?
    Initialization

---
+++ forBeginners/elif_statement/what_day.py
@@ -0,0 +1,34 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Exercise - What day is it? ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif statement: Exercise - What day is it?")
+
+
+# We consider the followings:
+#   - The reference time point is Tuesday, 10:30 in the morning 
+#     in London (UTC+00:00)
+#   - Read the input string containing the number and the sign of this 
+#     number (for example, +4, -10). Note, however, that there will be no sign 
+#     if the number is 0. The number is always an integer.
+#   - This number is the offset for some time zone.
+#   - Your program should calculate the day of the week in the time zone for 
+#     which you were given the offset. The reference time point for your 
+#     calculations is mentioned above.
+#   - Output the day of the week in the given time zone.
+#
+# The input format:
+#   The value of offset with the sign (e.g. +3 or -9). 
+#
+# The output format:
+#   The day of the week in that timezone.
+
+
+
+
+
+print_title("End of exercise")
