e2f573d7a436786d1b9e76930e504121b9f7b354
loicbaillif <larmedeciel@gmail.com>
2022-08-20 09:50:40 +0200

    PyForBeg/math/ex6/init
    Exercise 6: The logarithmic value
    Initialization

---
+++ forBeginners/math/logarithmic_value.py
@@ -0,0 +1,25 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - The logarithmic value ******
+
+# Global Python import
+import math
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions - Exercise: The logarithmic value")
+
+# Write a program that reads two integers (the first is always positive) 
+# and calculates the logarithmic value of the first integer with the second 
+# integer as a base. If the second integer is negative, 0 or 1, return the 
+# natural log of the first number.
+#
+# Use the function log() from the math module. With one argument, it returns 
+# the natural logarithm of a number. It can also take two arguments: 
+# a number and a logarithmic base.
+#
+# Print the result rounded to 2 decimal places.
+
+
+print_title("End of exercise")
