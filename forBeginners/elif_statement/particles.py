9117ff0082fa0b0c240cf9523955841f2834f288
loicbaillif <larmedeciel@gmail.com>
2022-08-23 07:04:43 +0200

    PyForBeg/elif/ex10/init
    Particles
    Initialization

---
+++ forBeginners/elif_statement/particles.py
@@ -0,0 +1,35 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Exercise - Particles ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif statement: Exercise - Particles")
+
+
+# The world of elementary particles is rather complex. There are many 
+# different classes and they can interact in a rather interesting way.
+#
+# Two important characteristics of the elementary particles are the spin 
+# and the electric charge. Here are some of the elementary particles:
+#
+#   Particle	Class	    Spin	Electric charge
+#   Strange	    Quark	    1/2	    -1/3
+#   Charm	    Quark	    1/2	    2/3
+#   Electron	Lepton	    1/2	    -1
+#   Neutrino	Lepton	    1/2	    0
+#   Photon	    Boson	    1	    0
+#  
+# Write a program that returns the particle and its class based on its spin and electric charge.
+#
+# The input format:
+#   Two lines: first the spin of the particle, then its charge. 
+#   You do NOT have to convert these values to floats.
+#
+# The output format:
+#   The particle and its class separated by a space.
+
+
+print_title("End of exercise")
