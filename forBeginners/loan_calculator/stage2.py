e968588d9a437c6a8443a308ec83d718adecdc20
loicbaillif <larmedeciel@gmail.com>
2022-08-24 00:43:10 +0200

    PyForBeg/loanCalc/stage2/init
    Project Loan Calculator: Stage 2
    Initialization

---
+++ forBeginners/loan_calculator/stage2.py
@@ -0,0 +1,23 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Loan calculator project - Stage 2 ******
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Loan calculator project: Stage 1 ".center(80, "*"))
+print(separator.center(80)+"\n")
+
+# DESCRIPTION
+# Let's consider 0% interest loans. We will also round all monthly payment, to 
+# get rid of cents.
+# It is also important to remember that no payment can be more than the fixed 
+# monthly payment.
+
+# OBJECTIVES
+# The behavior of your program should look like this:
+#       - Prompt a user to enter their loan principal and choose which of the 
+#       two parameters they want to calculate – the number of monthly payments 
+#       or the monthly payment amount.
+#       - To perform further calculations, you'll also have to ask for the 
+#       required missing value.
+#       - Finally, output the results for the user.
+
