f39438bfcb4e4af2044e325c1e35f5b9f08f3467
loicbaillif <larmedeciel@gmail.com>
2022-08-25 07:04:24 +0200

    PyForBeg/loadMod/ex5/init
    Load Module: Exercise 5
    Initialization

---
+++ forBeginners/load_module/exponent_x.py
@@ -0,0 +1,23 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Exercise - Particles ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Load modules: Exercise - exponent x minus 1")
+
+
+# Write a program that takes an integer x and prints e (a mathematical 
+# constant) raised to the power of x, minus one. 
+# Here's the formula: 
+#   (e^x -1)
+# 
+# Use the function expm1() defined in the math module. 
+# To do so, read its documentation carefully.
+
+
+
+
+print_title("End of exercise")
