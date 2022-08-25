5696c02c3d1845866f9bd442b88277d18967af8a
loicbaillif <larmedeciel@gmail.com>
2022-08-25 22:24:48 +0200

    PyForBeg/str_format/theory/v2
    Formatted string literals
    End of chapter;

--- forBeginners/string_formatting/theory.py
+++ forBeginners/string_formatting/theory.py
@@ -23,6 +23,26 @@ print("The {movie} movie at {theatre} was {adjective}.".
                theatre="Gaumont Champs-Elysées"))
 
 
+print_subtitle("Formatted string literals")
+name = "Elizabeth II"
+title = "Queen of the United Kingdom and the other Commonwealth realms"
+reign = "the longest-lived and the longest-reigning British monarch"
+print(f"{name}, the {title}, is {reign}.")
+
+bottle_price_in_cents = 2895
+bottle_name = "Chateau Latour"
+print(f"The {bottle_name} costs {bottle_price_in_cents / 100:.2f}€.")
+
+invite1 = "Please enter an integer number:\n> "
+invite2 = "Please enter the percentage you want from {}:\n> "
+invite3 = "Please enter the required number of decimals:\n> "
+hundred_percent_number = float(input(invite1))
+required_percentage = int(input(invite2.format(hundred_percent_number)))
+required_precision = int(input(invite3))
+user_result = hundred_percent_number * (required_percentage / 100.0)
+print((f"{required_percentage}% of {hundred_percent_number} "
+        f"(with {required_precision} digits) " 
+        f"equals {user_result:.{required_precision}f}"))
 
 
 print()
