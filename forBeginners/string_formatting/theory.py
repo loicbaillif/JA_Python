883902cac50a51bc65f84211876a231ee54a9ad4
loicbaillif <larmedeciel@gmail.com>
2022-08-25 19:25:37 +0200

    PyForBeg/str_format/theory/init
    String formatting: Theory
    Initialization

---
+++ forBeginners/string_formatting/theory.py
@@ -0,0 +1,15 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** String Formatting: Theory ******
+
+
+# Local application imports
+from print_title import print_title
+from print_title import print_subtitle
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("String Formatting")
+
+
+
+
+print_title("End of chapter")
