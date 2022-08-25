de1bfd01fd2172833aa27b970021754230ebec0e
loicbaillif <larmedeciel@gmail.com>
2022-08-25 19:38:34 +0200

    PyForBeg/str_format/theory/v1
    str.format() method
    Next: Formatted string literals

--- forBeginners/string_formatting/theory.py
+++ forBeginners/string_formatting/theory.py
@@ -9,7 +9,21 @@ from print_title import print_subtitle
 print_title("JetBrains Academy - Python for Beginners")
 print_title("String Formatting")
 
+print_subtitle("The str.format() method")
+print("Mix {}, {} and a {} to make an ideal omelet".
+        format("2 eggs", "30g of milk", "pinch of salt"))
+
+print("{0} in the {1}, by Frank Sinatra".format("Strangers", "Night"))
+print("{1} in the {0}, by Frank Sinatra".format("Strangers", "Night"))
+print("{1} in the {0}, by Frank Sinatra".format("Night", "Strangers"))
+
+print("The {movie} movie at {theatre} was {adjective}.".
+        format(movie="Mad Max: Fury Road", 
+               adjective="insane", 
+               theatre="Gaumont Champs-Elysées"))
 
 
 
+
+print()
 print_title("End of chapter")
