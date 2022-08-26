effb101857e83736c766b6c4316640a1bf236831
loicbaillif <larmedeciel@gmail.com>
2022-08-26 22:55:22 +0200

    PyForBeg/str_format/ex1/init
    Exercise 1: Film
    Initialization

---
+++ forBeginners/string_formatting/film.py
@@ -0,0 +1,21 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** String Formatting: Exercise - Film ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("String Formatting: Exercise - Film")
+
+
+# Write a program that gets information about a movie from the input 
+# and presents in the following format:
+#   movie (dir. director) came out in year
+#
+# The input format:
+#   3 lines: first the title of the movie, then the name of the director 
+#   and then the year of its release.
+
+
+print_title("End of exercise")
