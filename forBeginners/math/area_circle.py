fbafb6a308d3016d7fd6f835ead49536c0f3d37f
loicbaillif <larmedeciel@gmail.com>
2022-08-26 18:38:57 +0200

    PyForBeg/math/ex7/init
    Exercise: Area of a Circle
    Initialization

---
+++ forBeginners/math/area_circle.py
@@ -0,0 +1,21 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - Area of a Circle ******
+
+# Global Python import
+import math
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions - Exercise: Area of a circle")
+
+# Write a program that reads an integer that represents the radius of a given 
+# circle and calculates its area. To calculate the area of a circle, 
+# use the following formula: 
+#       S= pi * r²
+# Print the result rounded to 2 decimal places.
+
+
+
+print_title("End of exercise")
