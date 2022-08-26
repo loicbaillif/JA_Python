fbc3a0cc46192a5ded9ba2e814cbe6371e838a09
loicbaillif <larmedeciel@gmail.com>
2022-08-26 23:06:56 +0200

    PyForBeg/str_format/ex1/v2
    Final version, end of the exercise

--- forBeginners/string_formatting/film.py
+++ forBeginners/string_formatting/film.py
@@ -17,13 +17,12 @@ print_title("String Formatting: Exercise - Film")
 #   3 lines: first the title of the movie, then the name of the director 
 #   and then the year of its release.
 
-movie_title = input()
-movie_director = input()
-movie_release_year = input()
+movie_title = input("Please enter the movie title:\n> ")
+movie_director = input("Please enter the movie directir:\n> ")
+movie_release_year = input("Please enter the movie release year:\n> ")
 
 print(f"{movie_title} (dir. {movie_director}) "
       f"came out in {movie_release_year}")
-
 
 
 print_title("End of exercise")
