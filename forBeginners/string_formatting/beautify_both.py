3dacfa9819b32f4b2be486bf35cbc2d68d202212
loicbaillif <larmedeciel@gmail.com>
2022-08-27 10:39:39 +0200

    PyForBeg/str_format/ex2/init
    Exercise2: Beautify both output and code

---
+++ forBeginners/string_formatting/beautify_both.py
@@ -0,0 +1,31 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** String Formatting: Exercise - Beautify both output and code ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("String Formatting: Exercise - Beautify both input and output ******")
+
+
+# The output should be user-friendly, but the code part is also important. 
+# Well-structured and readable code is very important for being a good 
+# programmer. Now it's up to you to decide, which formatting method to choose.
+#
+# Imagine you need to compose a dynamic URL for every certain user with 
+# user-specific details. Suppose, you want to send different URLs for every 
+# user, depending on their name and profession. 
+# The base would be something like
+#
+#       "http://example.com/*nickname*/desirable/*profession*/profile"
+#  where nickname and profession are prompts from a user and are dynamic.
+#
+# Read the nickname and profession of the user from the input and print a 
+# user-specific URL. Don't bother about any rules of composing the URLs and 
+# just use raw input to accomplish the task.
+
+
+
+
+print_title("End of exercise")
