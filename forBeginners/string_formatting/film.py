719109c3642f0ae346e635dcab0603afd7a9ac14
loicbaillif <larmedeciel@gmail.com>
2022-08-27 10:32:36 +0200

    PyForBeg/str_format/ex1/v3
    Second version, using string.format() method.
    End of exercise

--- forBeginners/string_formatting/film.py
+++ forBeginners/string_formatting/film.py
@@ -11,18 +11,17 @@ print_title("String Formatting: Exercise - Film")
 
 # Write a program that gets information about a movie from the input 
 # and presents in the following format:
-#   movie (dir. director) came out in year XXXX
+#   movie (dir. director) came out in XXXX
 #
 # The input format:
 #   3 lines: first the title of the movie, then the name of the director 
 #   and then the year of its release.
 
 movie_title = input("Please enter the movie title:\n> ")
-movie_director = input("Please enter the movie directir:\n> ")
+movie_director = input("Please enter the movie director:\n> ")
 movie_release_year = input("Please enter the movie release year:\n> ")
 
-print(f"{movie_title} (dir. {movie_director}) "
-      f"came out in {movie_release_year}")
-
+print("{} (dir. {}) came out in {}".format(
+      movie_title, movie_director, movie_release_year))
 
 print_title("End of exercise")
