478000f7727bc38607867b31cedde04b71773bae
loicbaillif <larmedeciel@gmail.com>
2022-08-29 20:48:42 +0200

    PyForBeg/elif/ex11/init
    Elif: Exercise 11 - Coordinates
    Initialization

---
+++ forBeginners/elif_statement/coordinates.py
@@ -0,0 +1,30 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif: Exercise - Coordinates ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif: Exercise - Coordinates")
+
+
+
+# Locate a point on the Cartesian coordinate plane: 
+# which of four quadrants does a point belong to?
+#
+# Read two numbers from the input, not necessarily integers, the first number 
+# will indicate a position on the X-axis, the second one — on the Y-axis. 
+# Let's keep referring to the quadrants by Roman numerals, as detailed:
+#       I   = X > 0, Y > 0
+#       II  = X < 0, Y > 0
+#       III = X < 0, Y < 0
+#       IV  = X > 0, Y < 0
+#
+# If a point is the origin (0, 0), print "It's the origin!". In case of a 
+# point lying on the axes, with either x = 0 or y = 0, 
+# print "One of the coordinates is equal to zero!".
+
+
+
+print_title("End of exercise")
