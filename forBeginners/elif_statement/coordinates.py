f3789260c88df6672a5add4aefb2f449ab3728ca
loicbaillif <larmedeciel@gmail.com>
2022-08-29 20:55:06 +0200

    PyForBeg/elif/ex11/v1
    First proposition, to be tested

--- forBeginners/elif_statement/coordinates.py
+++ forBeginners/elif_statement/coordinates.py
@@ -25,6 +25,25 @@ print_title("elif: Exercise - Coordinates")
 # point lying on the axes, with either x = 0 or y = 0, 
 # print "One of the coordinates is equal to zero!".
 
+x_coordinate = float(input())
+y_coordinate = float(input())
+
+if x_coordinate * y_coordinate == 0:
+    if x_coordinate == y_coordinate:
+        print("It's the origin!")
+    else:
+        print("One of the coordinates is equal to zero!")
+elif x_coordinate > 0:
+    if y_coordinate > 0:
+        print("I")
+    else: 
+        print("IV")
+else:
+    if y_coordinate > 0:
+        print("II")
+    else:
+        print("III")
+
 
 
 print_title("End of exercise")
