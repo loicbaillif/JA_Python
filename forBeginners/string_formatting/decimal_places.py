7c69c574a2aafc4b7671742b61195855c0bebf29
loicbaillif <larmedeciel@gmail.com>
2022-08-30 13:45:24 +0200

    PyForBeg/str_format/ex3/init
    String Formatting: Exercise 3 - Decimal Places
    Initialization

---
+++ forBeginners/string_formatting/decimal_places.py
@@ -0,0 +1,24 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** String Formatting: Exercise - Decimal Places ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("String Formatting: Exercise - Decimal Places")
+
+
+# Round the number from input to the required number of decimals.
+
+# The input format:
+# Two lines: 
+#   the first with a floating-point number, 
+#   the second with an integer representing the decimal count.
+#
+# The output format:
+#   A formatted string containing the rounded number.
+
+
+
+print_title("End of exercise")
