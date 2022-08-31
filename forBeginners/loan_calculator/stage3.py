cafba5d43d59f8104a132200ccc24a1488823737
loicbaillif <larmedeciel@gmail.com>
2022-08-31 21:31:07 +0200

    PyForBeg/loanCalc/stage3/init
    Loan Calculator: Stage 3
    Initialization

---
+++ forBeginners/loan_calculator/stage3.py
@@ -0,0 +1,27 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Loan calculator project - Stage 3 ******
+
+# Global Python library import
+
+
+separator = "_"*40
+print(" JetBrains Academy - Python for Beginners ".center(80, "*"))
+print(" Loan calculator project: Stage 3 ".center(80, "*"))
+print(separator.center(80)+"\n")
+
+# DESCRIPTION
+# See the project page: https://hyperskill.org/projects/90/stages/502/implement
+
+
+# FUNCTIONS
+
+
+
+
+# VARIABLES
+
+
+
+
+# MAIN PROGRAM
+
