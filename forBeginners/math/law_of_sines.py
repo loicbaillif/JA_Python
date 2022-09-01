d3a5e5890961241d48583a9d0a0b4f68b715da95
loicbaillif <larmedeciel@gmail.com>
2022-09-01 07:03:18 +0200

    PyForBeg/math/ex8/init
    Math functions: Exercise - Law of Sines
    Initialization

---
+++ forBeginners/math/law_of_sines.py
@@ -0,0 +1,19 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - Law of Sines ******
+
+# Global Python import
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions - Exercise: Law of Sines")
+
+# There is a triangle on the picture below with the following parameters:
+#   - angle A and sides a and c are unknown;
+#   - angle B = 35°, angle C = 105°, side b = 7.
+# Find the side c using the math module. Print the answer rounded to 1 decimal place.
+
+
+print_title("End of exercise")
