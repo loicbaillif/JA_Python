24800edcbc2672367e34bf32c28a1adcca7c6dda
loicbaillif <larmedeciel@gmail.com>
2022-09-04 22:51:21 +0200

    PyForBeg/for/theory/v1
    First section, first examples

--- forBeginners/for/theory.py
+++ forBeginners/for/theory.py
@@ -11,6 +11,13 @@ from print_title import print_subtitle
 print_title("JetBrains Academy - Python for Beginners")
 print_title("For loop")
 
+print_subtitle("for loop syntax")
+oceans = ["Arctic", "Atlantic", "Indian", "Pacific", "Southern"]
+for ocean in oceans:
+    print(ocean)
+
+for letter in "Superbus":
+    print(letter)
 
 
 print_title("End of chapter")
