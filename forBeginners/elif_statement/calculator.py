99c9db61d1ca6a959cd479578cc095b148675d1c
loicbaillif <larmedeciel@gmail.com>
2022-09-05 21:01:47 +0200

    PyForBeg/elif/ex12/init
    Exercise 12: Calculator
    Initialization

---
+++ forBeginners/elif_statement/calculator.py
@@ -0,0 +1,24 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Exercise - Calculator ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif statement: Exercise - Calculator")
+
+# Let's write a simple calculator!
+# 
+# It will read 3 lines:
+#   - the first number
+#   - the second number
+#   - the arithmetic operation.
+# Numbers are floats!
+#
+# The output is the result of the following: first_number operation second_number.
+#
+# Operations are: +, -, /, *, mod, pow, div.
+
+
+print_title("End of exercise")
