f10cf37f2871640aee5822dc2d9214292b051065
loicbaillif <larmedeciel@gmail.com>
2022-09-06 19:09:05 +0200

    PyForBeg/for/ex1/init
    for loop: Exercise 1 - Lucky 7
    Initialization

---
+++ forBeginners/for/lucky7.py
@@ -0,0 +1,21 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** for loop: Exercise - Lucky 7 ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("for loop: Exercise - Lucky 7")
+
+
+# https://hyperskill.org/learn/step/6549
+# Read the first input – a number n. Then read input n times. 
+# Each time you'll get an integer value. If it's a multiple of 
+# 7 (can be divided by 7 without a remainder), print its square on a new line. 
+# Note that you don't need to perform any calculations 
+# on the first input (the n).
+
+
+
+print_title("End of exercise")
