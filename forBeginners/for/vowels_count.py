f1049692af5f2c7e38f7e6900df0e053830debf9
loicbaillif <larmedeciel@gmail.com>
2022-09-08 17:16:20 +0200

    PyForBeg/for/ex2/init
    For loop exercise - Vowels count
    Initialization

---
+++ forBeginners/for/vowels_count.py
@@ -0,0 +1,18 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** for loop: Exercise - Vowels count ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("for loop: Exercise - Vowels count")
+
+
+# https://hyperskill.org/learn/step/7199
+# You have a predefined string vowels that contains all letters designating 
+# vowel sounds. Write a program that counts the number of vowels in the 
+# variable string and prints this number.
+
+
+print_title("End of exercise")
