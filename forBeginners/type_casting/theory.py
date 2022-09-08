9a9d51eeb291de731af2402995b53731e215e2d1
loicbaillif <larmedeciel@gmail.com>
2022-09-08 20:48:09 +0200

    PyForBeg/typeCasting/theory/init

---
+++ forBeginners/type_casting/theory.py
@@ -0,0 +1,17 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Type Casting: Theory ******
+
+# Global Python import
+
+
+# Local application imports
+from print_title import print_title
+from print_title import print_subtitle
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Type Casting")
+
+
+
+
+print_title("End of chapter")
