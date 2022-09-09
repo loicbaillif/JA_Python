8d5a7ede581f0fe331af1c6fa150d234beb0cb00
loicbaillif <larmedeciel@gmail.com>
2022-09-09 19:29:26 +0200

    PyForBeg/typeCasting/ex1/init
    Type Casting - Exercise 1 : Exactly 100 times
    Initialization

---
+++ forBeginners/type_casting/hundred_times.py
@@ -0,0 +1,19 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Type casting: Exercise - Exactly 100 times ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Type casting: Exercise - Exactly 100 times")
+
+
+# https://hyperskill.org/learn/step/7199
+# Jane knows that the variable n stores some integer number (for example, 
+# 12345) and wants to print it exactly 100 times in one line. Help her and 
+# write down a single line of code that will print number n exactly 100 times.
+
+
+
+print_title("End of exercise")
