b3260801f48308c9fc2462e8a317124bef8e852c
loicbaillif <larmedeciel@gmail.com>
2022-09-10 05:14:42 +0200

    PyForBeg/errors/ex1/init
    Errors - Exercise : Chaos
    Initialization

---
+++ forBeginners/errors/chaos.py
@@ -0,0 +1,20 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Errors: Exercise - Chaos ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Errors: Exercise - Chaos")
+
+
+# https://hyperskill.org/learn/step/8310
+# Look at the code below and eliminate the chaos: the first line should print 
+# the resulting number of the calculation and the second line should print 
+# the word mathematics.
+
+print("45/9 + 16*(5 + 8)")
+print(mathematics)
+
+print_title("End of exercise")
