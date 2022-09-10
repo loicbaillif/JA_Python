0f60b2c92a4cc308d47688309c798d1621b13a0a
loicbaillif <larmedeciel@gmail.com>
2022-09-10 21:58:39 +0200

    PyForBeg/for/ex3/init
    for loop - exercise: The average of all numbers
    Initialization

---
+++ forBeginners/for/average.py
@@ -0,0 +1,19 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** for loop: Exercise - Average of all numbers ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("for loop: Exercise - Average of all numbers")
+
+
+# https://hyperskill.org/learn/step/6071
+# Write a program that reads 2 numbers, a and b, from the input. As an output, 
+# it shows the mean of all numbers that lie on the interval between 
+# a and b (included), and are divisible by 3.
+
+
+
+print_title("End of exercise")
