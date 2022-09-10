ffd5e5af9428eb383a7570bd29a23f8c5fcbdac6
loicbaillif <larmedeciel@gmail.com>
2022-09-10 05:08:11 +0200

    PyForBeg/errors/theory/v2
    Common errors for dummies + example

--- forBeginners/errors/theory.py
+++ forBeginners/errors/theory.py
@@ -16,6 +16,21 @@ print("print('Hello, World!'")
 print("Will generate a Syntax Error, completed by 'unexpected EOF")
 print("EOF = End Of File")
 
+print_subtitle("Common errors for dommies")
+print("Wrong spelling: pint, wjile, For, ...")
+print("Parentheses")
+print("indentation")
+print("Quotation marks")
+print("*** Example:")
+# missing_quote  = "this is a mistake!
+# another_string = this is not a string!"
+# parted_string = 'I'd like to be highlighted, but'
+#           prnit("I am not")
+missing_quote  = "this is a mistake!"
+another_string = "this is not a string!"
+parted_string = 'I\'d like to be highlighted, but'
+print("I am not")
+
 
 
 print_title("End of chapter")
