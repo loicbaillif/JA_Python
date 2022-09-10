beff3aff13ef3d3204e76eff0c053b5dd28fdefe
loicbaillif <larmedeciel@gmail.com>
2022-09-10 04:29:17 +0200

    PyForBeg/errors/theory/v1
    Syntax Errors

--- forBeginners/errors/theory.py
+++ forBeginners/errors/theory.py
@@ -11,6 +11,11 @@ from print_title import print_subtitle
 print_title("JetBrains Academy - Python for Beginners")
 print_title("Errors")
 
+print_subtitle("Syntax errors")
+print("print('Hello, World!'")
+print("Will generate a Syntax Error, completed by 'unexpected EOF")
+print("EOF = End Of File")
+
 
 
 print_title("End of chapter")
