619af321e9d111ceeb33332ab1c29417794275f7
loicbaillif <larmedeciel@gmail.com>
2022-09-10 04:25:02 +0200

    PyForBeg/errors/theory/init
    Errors: Theory
    Initialization

---
+++ forBeginners/errors/theory.py
@@ -0,0 +1,16 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Errors: Theory ******
+
+# Global Python import
+
+
+# Local application imports
+from print_title import print_title
+from print_title import print_subtitle
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Errors")
+
+
+
+print_title("End of chapter")
