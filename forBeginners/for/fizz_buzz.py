dae72f4cae9a6936fbaed487ac2d25926ca004c7
loicbaillif <larmedeciel@gmail.com>
2022-09-13 07:08:27 +0200

    PyForBeg/for/ex4/v1
    First version, not tested yet

--- forBeginners/for/fizz_buzz.py
+++ forBeginners/for/fizz_buzz.py
@@ -21,4 +21,14 @@ print_title("for loop: Exercise - FizzBuzz")
 #
 # Output each value on a separate line.
 
+for number in range(101):
+    if (number % 3 == 0 and number % 5 == 0):
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else: 
+        print(number)
+
 print_title("End of exercise")
