9f3e2329f2486561a1756ea51ce5c39c8668f85c
loicbaillif <larmedeciel@gmail.com>
2022-09-14 07:08:49 +0200

    PyForBeg/errors/ex2/init
    Errors: Exercise - English Contractions
    Initialization

---
+++ forBeginners/errors/english_contractions.py
@@ -0,0 +1,26 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Errors: Exercise - English Contractions ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Errors: Exercise - English Contractions")
+
+
+# https://hyperskill.org/learn/step/8311
+# Lucy is drawing a poster with English contractions for her students. 
+# We will join her English club and help with learning materials.
+#
+# Print the following strings with contractions:
+#   - I am = I'm
+#   - I have = I've
+#   - I will = I'll
+#   -   I had / would = I'd
+# Each string should be on a new line. This will be easy 
+# if you use quotes wisely.
+
+
+
+print_title("End of exercise")
