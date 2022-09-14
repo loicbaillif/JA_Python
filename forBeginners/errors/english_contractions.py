b9d2f15f50c2e25930df6f65a38b46eea8de06f4
loicbaillif <larmedeciel@gmail.com>
2022-09-14 07:11:14 +0200

    PyForBeg/errors/ex2/v1
    First version, tested and validated
    Next: try a second version

--- forBeginners/errors/english_contractions.py
+++ forBeginners/errors/english_contractions.py
@@ -17,10 +17,13 @@ print_title("Errors: Exercise - English Contractions")
 #   - I am = I'm
 #   - I have = I've
 #   - I will = I'll
-#   -   I had / would = I'd
+#   - I had / would = I'd
 # Each string should be on a new line. This will be easy 
 # if you use quotes wisely.
 
-
+print("I am = I'm")
+print("I have = I've")
+print("I will = I'll")
+print("I had / would = I'd")
 
 print_title("End of exercise")
