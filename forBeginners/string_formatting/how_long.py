c915aa8fcd8a4f22ebe3b8cebaecd6277865139e
loicbaillif <larmedeciel@gmail.com>
2022-09-15 08:10:34 +0200

    PyForBeg/str_format/ex4/init
    String Formatting: Exercise - How long is that word?
    Initialization

---
+++ forBeginners/string_formatting/how_long.py
@@ -0,0 +1,19 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** String Formatting: Exercise - How long is that word? ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("String Formatting: Exercise - How long is that word?")
+
+
+# https://hyperskill.org/learn/step/6896
+# Write a program that calculates the length of a word from the input and 
+# prints it out together with the word in the format word has N letters. 
+# There will always be more than one letter in the word.
+
+
+
+print_title("End of exercise")
