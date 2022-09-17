9e5aa916d3baae2b229f86c6dd7db004b52a4eba
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-09-17 11:34:37 +0200

    PyForBeg/math/ex9/v1
    First version

--- forBeginners/math/logistic.py
+++ forBeginners/math/logistic.py
@@ -1,6 +1,8 @@
 #  ******** JetBrains Academy - Python for Beginners ********
 #  ****** Math functions: Exercise - Logistic functions ******
 
+# Global Python import
+from math import exp
 
 # Local application imports
 from print_title import print_title
@@ -15,6 +17,10 @@ print_title("Math functions: Exercise - Logistic functions")
 #   sigma(x) = 1 / (1 + e^(-x)) = e^x / (e^x + 1)
 #
 # Print the result rounded to 2 decimal places.
+
+user_input = int(input())
+logistic_user_input = exp(user_input) / (exp(user_input) + 1)
+print(round(logistic_user_input, 2))
 
 
 print_title("End of exercise")
