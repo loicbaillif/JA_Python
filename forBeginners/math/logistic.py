fbc006f20de95c623d7d18ca1997d8a51f350e10
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-09-17 11:24:53 +0200

    PyForBeg/math/ex9/init
    Math functions - Exercise: Logistic function
    Initialization

---
+++ forBeginners/math/logistic.py
@@ -0,0 +1,20 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - Logistic functions ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions: Exercise - Logistic functions")
+
+# https://hyperskill.org/learn/step/6348
+# Write a program that reads an integer and calculates the value of its 
+# logistic function. A logistic function, or sigmoid function, is a function 
+# defined by the formula 
+#   sigma(x) = 1 / (1 + e^(-x)) = e^x / (e^x + 1)
+#
+# Print the result rounded to 2 decimal places.
+
+
+print_title("End of exercise")
