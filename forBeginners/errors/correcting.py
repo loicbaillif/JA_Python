a9f162807a8033f43aa5ff7d7c12977a7a02407c
loicbaillif <larmedeciel@gmail.com>
2022-09-18 21:17:51 +0200

    PyForBeg/errors/ex3/v1
    First proposal

--- forBeginners/errors/correcting.py
+++ forBeginners/errors/correcting.py
@@ -14,8 +14,8 @@ print_title("Errors: Exercise - Correcting the mistakes")
 # according to Python's syntax? Try to fix all mistakes, so that SyntaxError 
 # will not be raised during the initialization of these variables.
 
-hope = "I hope this is actually going to work'
+hope = 'I hope this is actually going to work'
 doubt = 'Do I need to double check all lines?'
-suggestion = "What if I write it in "double quotes"?
+suggestion = 'What if I write it in "double quotes"?'
 
 print_title("End of exercise")
