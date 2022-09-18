c3aa0e54c8da5cc6dbbd008431809052f78b655f
loicbaillif <larmedeciel@gmail.com>
2022-09-18 21:16:26 +0200

    PyForBeg/errors/ex3/init
    Errors - Exercise 3: Correcting the mistakes
    Initialization

---
+++ forBeginners/errors/correcting.py
@@ -0,0 +1,21 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Errors: Exercise - Correcting the mistakes ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Errors: Exercise - Correcting the mistakes")
+
+
+# https://hyperskill.org/learn/step/11136
+# Take a look at the following code. Did we declare all variables correctly, 
+# according to Python's syntax? Try to fix all mistakes, so that SyntaxError 
+# will not be raised during the initialization of these variables.
+
+hope = "I hope this is actually going to work'
+doubt = 'Do I need to double check all lines?'
+suggestion = "What if I write it in "double quotes"?
+
+print_title("End of exercise")
