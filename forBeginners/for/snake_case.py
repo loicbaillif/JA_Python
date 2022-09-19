352284b0c003617265e9810feb7cbef56afa3b46
loicbaillif <larmedeciel@gmail.com>
2022-09-19 07:40:16 +0200

    PyForBeg/for/ex5/init
    for loop - exercise : Snake case
    Initialization

---
+++ forBeginners/for/snake_case.py
@@ -0,0 +1,27 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** for loop: Exercise - snake_case ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("for loop: Exercise - snake_case")
+
+
+# https://hyperskill.org/learn/step/9476
+# Since we are learning Python, sometimes it might be useful to convert text 
+# from lowerCamelCase to snake_case. The main trick is to find the correct 
+# place where to insert an underscore. Let's make a rule that an underscore 
+# should be inserted right before a capital letter, so lowerCamelCase would 
+# become lower_camel_case.
+#
+# The input format:
+#   Read a word or a phrase written in lowerCamelCase
+#
+# The output format:
+#   Print out words in lowercase and separate them by underscores.
+
+
+
+print_title("End of exercise")
