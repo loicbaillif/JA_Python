fdb2ab95bd4693d17946bf4c6873b488d8e83fff
loicbaillif <larmedeciel@gmail.com>
2022-09-21 07:06:30 +0200

    PyForBeg/elif/ex14/init
    elif statement: Exercise - Long live the king
    Initialization

---
+++ forBeginners/elif_statement/long_live.py
@@ -0,0 +1,28 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** elif statement: Exercise - Long live the king ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("elif statement: Exercise - Long live the king")
+
+# https://hyperskill.org/learn/step/8441
+# Let's imagine a chessboard, the squares on which are marked with coordinates. 
+# The coordinates are numbers between 1 and 8 inclusively. The first number 
+# indicates a column, the second one indicates a row.
+#
+# The chess king can stand on any square and can move one step horizontally, 
+# vertically, or diagonally in any direction within the board.
+#
+# The input will contain the coordinates on which the king is located. 
+# You should figure out and print how many moves the figure can make: 
+# for example, from the position (1, 8), the king can make 
+# only 3 moves (right, down, diagonally).
+
+
+
+
+
+print_title("End of exercise")
