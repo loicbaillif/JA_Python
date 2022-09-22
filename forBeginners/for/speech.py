9e262ccb6cb98b2ff57ef75c04158be4889d8fc5
loicbaillif <larmedeciel@gmail.com>
2022-09-22 06:49:09 +0200

    PyForBeg/for/ex6/v1
    Initialization with subject

---
+++ forBeginners/for/speech.py
@@ -0,0 +1,19 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** for loop: Exercise - Speech generation ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("for loop: Exercise - Speech generation")
+
+# https://hyperskill.org/learn/step/8438
+# Here is your chance to write instructions for a text-to-speech system. 
+# Let's focus on reading phone numbers aloud. The input phone number will 
+# consist solely of digits. Print the name of each digit on a new line for 
+# the system to read them one by one.
+
+
+
+print_title("End of exercise")
