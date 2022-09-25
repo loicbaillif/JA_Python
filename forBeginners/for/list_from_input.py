d95012f006c2459204e0d7478cce8d114444bff5
loicbaillif <larmedeciel@gmail.com>
2022-09-25 11:46:08 +0200

    PyForBeg/for/ex7/init
    Exercise 7: List from input
    File initialization

---
+++ forBeginners/for/list_from_input.py
@@ -0,0 +1,21 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** for loop: Exercise - List from input ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("for loop: Exercise - List from input")
+
+# https://hyperskill.org/learn/step/6739
+# Use the template below to write a program that takes n numbers as input, 
+# creates a list from them, and then prints it.
+#
+# The first number in the sample input shows how many elements your list 
+# should contain. The numbers after it are the elements that you need 
+# to append to your list.
+
+
+
+print_title("End of exercise")
