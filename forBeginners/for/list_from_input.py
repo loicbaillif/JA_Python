afbdcf568fb82a3c26fb4e796ead946bab1c90cb
loicbaillif <larmedeciel@gmail.com>
2022-09-25 12:19:41 +0200

    PyForBeg/for/ex7/v1
    First version

--- forBeginners/for/list_from_input.py
+++ forBeginners/for/list_from_input.py
@@ -16,6 +16,11 @@ print_title("for loop: Exercise - List from input")
 # should contain. The numbers after it are the elements that you need 
 # to append to your list.
 
+nb_elements = int(input())
+list_elements = []
+for _ in range(nb_elements):
+    list_elements.append(int(input()))
 
+print(list_elements)
 
 print_title("End of exercise")
