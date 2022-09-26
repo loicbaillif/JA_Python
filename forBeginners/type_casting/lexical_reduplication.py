e48d7dd98dd3884a726fdd875d1271f5e3985639
loicbaillif <larmedeciel@gmail.com>
2022-09-26 08:40:56 +0200

    PyForBeg/typeCasting/ex2/init
    Exercise 2: Lexical reduplication
    Initialization

---
+++ forBeginners/type_casting/lexical_reduplication.py
@@ -0,0 +1,25 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Type casting: Exercise - Lexical reduplication ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Type casting: Exercise - Lexical Reduplication")
+
+
+# https://hyperskill.org/learn/step/8213
+# The languages of the world are amazing! Programming languages too, but now
+# we will talk about the human ones. In linguistics, repeating a word or part
+# of it is called reduplication. This morphological phenomenon is found in
+# different languages. Think for a second, and you will definitely come up 
+# with a couple of examples. Just to name a few: knock-knock, so-so, bye-bye.
+#
+# We have a full reduplication here since the entire word is repeated. That's 
+# the mechanism we want you to implement. Print a word exactly 2 times. 
+# The spelling rules vary across the globe, so do not separate the halves.
+
+
+
+print_title("End of exercise")
