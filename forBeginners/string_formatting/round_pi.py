1ecb9cdcced670908b7c9706a4585f16b17ede1f
loicbaillif <larmedeciel@gmail.com>
2022-09-27 07:07:36 +0200

    PyForBeg/str_format/ex5/init
    Exercise: Round pi
    Initialization

---
+++ forBeginners/string_formatting/round_pi.py
@@ -0,0 +1,17 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** String Formatting: Exercise - Round pi ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("String Formatting: Exercise - Round pi")
+
+
+# https://hyperskill.org/learn/step/6900
+# Write a code that rounds the number pi to 5 decimal places and prints it out.
+
+
+
+print_title("End of exercise")
