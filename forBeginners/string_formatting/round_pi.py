27cd352dee51ee8d189ab73ac8f09d538a64491a
loicbaillif <larmedeciel@gmail.com>
2022-09-27 07:10:05 +0200

    PyForBeg/str_format/ex5/v1
    First version: Tested and validated
    End of exercise

--- forBeginners/string_formatting/round_pi.py
+++ forBeginners/string_formatting/round_pi.py
@@ -12,6 +12,7 @@ print_title("String Formatting: Exercise - Round pi")
 # https://hyperskill.org/learn/step/6900
 # Write a code that rounds the number pi to 5 decimal places and prints it out.
 
-
+pi = 3.141592653589793
+print(f"{pi:.5f}")
 
 print_title("End of exercise")
