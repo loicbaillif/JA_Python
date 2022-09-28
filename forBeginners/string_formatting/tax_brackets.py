9dc1cb6c12b63aa2bfe61a9a1e4c1fcb04e3c1df
loicbaillif <larmedeciel@gmail.com>
2022-09-28 07:09:15 +0200

    PyForBeg/str_format/ex6/init
    Exercise: Tax brackets
    Initialization

---
+++ forBeginners/string_formatting/tax_brackets.py
@@ -0,0 +1,35 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** String Formatting: Exercise - Tax brackets ******
+
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("String Formatting: Exercise - Tax brackets")
+
+
+# https://hyperskill.org/learn/step/6519
+# In progressive tax systems, tax rates change according to the income. 
+# Tax brackets are divisions that regulate those changes.
+# Here's an example of tax brackets in a certain tax system:
+#       - 0 — 15,527: 0% tax
+#       - 15,528 — 42,707: 15% tax
+#       - 42,708 — 132,406: 25% tax
+#       - 132,407 and more: 28% tax
+# 
+# Suppose we use a simplified version of taxation and apply one tax rate 
+# to the entire amount of money.
+# Write a program that calculates the tax that a person's going to pay 
+# based on their income.
+#
+# The input format:
+#   The value of someone's taxable income (in dollars).
+# The output format:
+#   The tax for {income} is {percent}%. That is {calculated_tax} dollars!
+# 
+# Round your calculated_tax to the nearest integer.
+
+
+
+print_title("End of exercise")
