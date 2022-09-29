c676e3a4c6b7ded970c3a69408b10aa64e3726a9
loicbaillif <larmedeciel@gmail.com>
2022-09-29 10:34:04 +0200

    PyForBeg/math/ex10/init
    Exercise: Octahedron
    Initialization

---
+++ forBeginners/math/octahedron.py
@@ -0,0 +1,32 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Math functions: Exercise - Octahedron ******
+
+# Global Python import
+from math import exp
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Math functions: Exercise - Octahedron")
+
+# https://hyperskill.org/learn/step/6346
+# A regular octahedron is a three-dimensional geometric shape, a polyhedron 
+# with eight equal faces and twelve equal edges. Have you ever seen 8-sided 
+# dice? That's a regular octahedron. In this task you need to calculate its 
+# area and volume.
+#
+# Write a program that reads a positive integer representing the edge length 
+# of the regular octahedron and prints two values – its area and volume 
+# (in that order), both rounded to 2 decimal places and split by one space.
+#
+# Formula to calculate the area of an octahedron (a is edge length):
+#   2 * sqrt{3} * a ^ 2
+#
+# Formula to calculate the volume:
+#   (1/3) * sqrt{2} * a ^ 3
+
+
+
+
+print_title("End of exercise")
