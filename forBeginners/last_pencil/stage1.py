8a9263cb077ecf6a730cd36063abbf444f7f21ec
loicbaillif <larmedeciel@gmail.com>
2023-02-11 22:32:33 +0100

    PyForBeg/last_pencil/st0/v1 First output

--- forBeginners/last_pencil/stage1.py
+++ forBeginners/last_pencil/stage1.py
@@ -1,3 +1,7 @@
 # ********** JetBrains Academy - Python for Beginners **********
 # ******** Project - Last Pencil : Stage 1 / 5 ********
 
+# https://hyperskill.org/projects/258/stages/1302/implement
+
+print('|' * 10)
+
