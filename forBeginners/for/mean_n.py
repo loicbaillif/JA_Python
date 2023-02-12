1f62593ab29169df2966ea8ce7b98dbf5ea1843b
loicbaillif <larmedeciel@gmail.com>
2023-02-12 10:46:16 +0100

    PyForBeg/for/ex8/v0 A mean of n - init

---
+++ forBeginners/for/mean_n.py
@@ -0,0 +1,13 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** for loop: Exercise - A mean of n ******
+# https://hyperskill.org/learn/daily/6818
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("for loop: Exercise - A mean of n")
+
+
+
+print_title("End of exercise")
