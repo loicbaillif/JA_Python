56ab10da44cc600e3514bd74cbfa82a96c30cf3e
loicbaillif <larmedeciel@gmail.com>
2023-02-14 07:32:01 +0100

    PyForBeg/simple/ex1/v2 Second proposal

--- forBeginners/simple_programs/a_week.py
+++ forBeginners/simple_programs/a_week.py
@@ -8,20 +8,6 @@ from print_title import print_title
 print_title("JetBrains Academy - Python for Beginners")
 print_title("Simple Programs: Exercise - A week")
 
-print("""
-Sunday
-
-Monday
-
-Tuesday
-
-Wednesday
-
-Thursday
-
-Friday
-
-Saturday
-""")
+print("Sunday\n\nMonday\n\nTuesday\n\nWednesday\n\nThursday\n\nFriday\n\nSaturday")
 
 print_title("End of exercise")
