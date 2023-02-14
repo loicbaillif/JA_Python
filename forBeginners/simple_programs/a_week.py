af03cad221131777f1cc3b52fd39df80ac27a0fd
loicbaillif <larmedeciel@gmail.com>
2023-02-14 07:29:49 +0100

    PyForBeg/simple/ex1/v0 Simple Programs - Exercise 1 : init

---
+++ forBeginners/simple_programs/a_week.py
@@ -0,0 +1,12 @@
+#  ******** JetBrains Academy - Python for Beginners ********
+#  ****** Simple Programs: Exercise - A week ******
+#  https://hyperskill.org/learn/daily/22849
+
+# Local application imports
+from print_title import print_title
+
+print_title("JetBrains Academy - Python for Beginners")
+print_title("Simple Programs: Exercise - A week")
+
+
+print_title("End of exercise")
