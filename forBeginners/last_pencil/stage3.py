31d3db110be880fca5c922849900b9b3f496a045
loicbaillif <larmedeciel@gmail.com>
2023-07-03 23:05:03 +0200

    PyForBeg/lastPencil/st3/v7 Debug

--- forBeginners/last_pencil/stage3.py
+++ forBeginners/last_pencil/stage3.py
@@ -3,7 +3,7 @@
 # ******** https://hyperskill.org/projects/258/stages/1304/implement ********
 
 def play_round():
-    pick = int(input(f"{list_players[round + index_first_player]}'s turn:\n"))
+    pick = int(input(f"{list_players[(round + index_first_player) % 2]}'s turn:\n"))
     return pick
 
 nb_pencils = int(input("How many pencils would you like to use:\n"))
@@ -11,10 +11,12 @@ first_player = input("Who will be the first (Davy, Jones):\n")
 list_players = ["Davy", "Jones"]
 round = 0
 index_first_player = list_players.index(first_player)
-print('|' * nb_pencils)
+
 
 while nb_pencils != 0:
-    nb_pencils = nb_pencils - play_round()
+    print('|' * nb_pencils)
+    nb_pencils -= play_round()
+    round += 1
 
 
 
