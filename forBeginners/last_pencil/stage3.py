1f6aa146f09c35df2214069d265ba92ccbfa23c9
loicbaillif <larmedeciel@gmail.com>
2023-07-03 22:45:13 +0200

    PyForBeg/lastPencil/st3/v1 Get from stage 2

--- forBeginners/last_pencil/stage3.py
+++ forBeginners/last_pencil/stage3.py
@@ -1,3 +1,8 @@
 # ********** JetBrains Academy - Python for Beginners **********
 # ******** Project - Last Pencil : Stage 3 / 5 ********
 # ******** https://hyperskill.org/projects/258/stages/1304/implement ********
+
+nb_pencils = int(input("How many pencils would you like to use:\n"))
+first_player = input("Who will be the first (Davy, Jones):\n")
+
+print('|' * nb_pencils)
