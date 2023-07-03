126dc92397a1fd046431ceb5e1379ce4e3e3614d
loicbaillif <larmedeciel@gmail.com>
2023-07-03 22:33:34 +0200

    PyForBeg/lastPencil/st2/v2 Get first player

--- forBeginners/last_pencil/stage2.py
+++ forBeginners/last_pencil/stage2.py
@@ -3,4 +3,4 @@
 # ******** https://hyperskill.org/projects/258/stages/1303/implement ********
 
 nb_pencils = int(input("How many pencils would you like to use:\n"))
-
+first_player = input("Who will be the first (Davy, Jones):")
