26aa501e79849bf3624a3e4d0c7a206a37ac6020
loicbaillif <larmedeciel@gmail.com>
2023-07-03 23:02:07 +0200

    PyForBeg/lastPencil/st3/v6 Define main loop

--- forBeginners/last_pencil/stage3.py
+++ forBeginners/last_pencil/stage3.py
@@ -2,16 +2,19 @@
 # ******** Project - Last Pencil : Stage 3 / 5 ********
 # ******** https://hyperskill.org/projects/258/stages/1304/implement ********
 
+def play_round():
+    pick = int(input(f"{list_players[round + index_first_player]}'s turn:\n"))
+    return pick
+
 nb_pencils = int(input("How many pencils would you like to use:\n"))
 first_player = input("Who will be the first (Davy, Jones):\n")
 list_players = ["Davy", "Jones"]
 round = 0
 index_first_player = list_players.index(first_player)
-
-
 print('|' * nb_pencils)
 
+while nb_pencils != 0:
+    nb_pencils = nb_pencils - play_round()
 
-def play_round():
-    pick = int(input(f"{list_players[round + index_first_player]}'s turn:\n"))
-    return pick
+
+
