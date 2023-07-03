cdcf126941f5aed54aca8eb7499e07db45420fde
loicbaillif <larmedeciel@gmail.com>
2023-07-03 22:30:51 +0200

    PyForBeg/lastPencil/st2/v1 Get initial number of pencils from user

--- forBeginners/last_pencil/stage2.py
+++ forBeginners/last_pencil/stage2.py
@@ -2,4 +2,5 @@
 # ******** Project - Last Pencil : Stage 2 / 5 ********
 # ******** https://hyperskill.org/projects/258/stages/1303/implement ********
 
+nb_pencils = int(input("How many pencils would you like to use:\n"))
 
