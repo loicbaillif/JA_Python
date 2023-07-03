27c44c92c62e8f9b3fcb4a3b1311aa85f8f78eaf
loicbaillif <larmedeciel@gmail.com>
2023-07-03 22:30:28 +0200

    PyForBeg/lastPencil/st2/init Stage 2: new rules

---
+++ forBeginners/last_pencil/stage2.py
@@ -0,0 +1,5 @@
+# ********** JetBrains Academy - Python for Beginners **********
+# ******** Project - Last Pencil : Stage 2 / 5 ********
+# ******** https://hyperskill.org/projects/258/stages/1303/implement ********
+
+
