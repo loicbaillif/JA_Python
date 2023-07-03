571ebb30ae72b1a48765a7655ce5cdac379e5917
loicbaillif <larmedeciel@gmail.com>
2023-07-03 22:44:34 +0200

    PyForBeg/lastPencil/st3/init Working on the gameplay

---
+++ forBeginners/last_pencil/stage3.py
@@ -0,0 +1,3 @@
+# ********** JetBrains Academy - Python for Beginners **********
+# ******** Project - Last Pencil : Stage 3 / 5 ********
+# ******** https://hyperskill.org/projects/258/stages/1304/implement ********
