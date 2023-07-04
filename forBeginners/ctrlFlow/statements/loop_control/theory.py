f457369441624b38c05c85ccbe160ff23e9612bd
loicbaillif <larmedeciel@gmail.com>
2023-07-04 17:25:21 +0200

    PyForBeg/loop_ctrl_stat/th/init Loop control statement

---
+++ forBeginners/ctrlFlow/statements/loop_control/theory.py
@@ -0,0 +1,10 @@
+# Theory: Loop control statement
+# https://hyperskill.org/learn/step/6302
+# Author: JetBrains Academy
+
+print("***** Theory: Loop control statement *****")
+
+
+
+
+print("\n***** End of theory *****")
