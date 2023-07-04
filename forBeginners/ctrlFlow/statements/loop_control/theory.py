53474c65b54859611d376208b48f333335df5af4
loicbaillif <larmedeciel@gmail.com>
2023-07-04 17:49:09 +0200

    PyForBeg/loop_ctrl_stat/th/v1 break example

--- forBeginners/ctrlFlow/statements/loop_control/theory.py
+++ forBeginners/ctrlFlow/statements/loop_control/theory.py
@@ -4,7 +4,13 @@
 
 print("***** Theory: Loop control statement *****")
 
-
+print("\n*** 1) break")
+pets = ["dog", "cat", "bonobo"]
+for pet in pets:
+    print(pet)
+    if pet == "cat":
+        print("Loop break")
+        break
 
 
 print("\n***** End of theory *****")
